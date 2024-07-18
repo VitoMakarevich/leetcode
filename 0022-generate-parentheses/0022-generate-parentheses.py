730a2aaa8dff0702315519be5ec1f4a5a45dab52
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-07-18 16:13:35 +0200

    Time: 44 ms (18.33%), Space: 16.9 MB (11.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,27 +2,29 @@ import io
 
 class Solution:
 
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        buf = io.StringIO()
-        self._rec(buf, 2 * n, 0, n, res, False)
-        return res
-        
-    def _rec(self, buf, target_l, left_open, allowed_to_open, res, last_left):
-        if len(buf.getvalue()) == target_l:
-            res.append(buf.getvalue())
-            return
-        if not last_left:
-            for i in range(1, allowed_to_open + 1):
-                for o in range(i):
-                    buf.write('(')
-                self._rec(buf, target_l, left_open + i, allowed_to_open - i, res, True)
-                buf.seek(len(buf.getvalue()) - i)  # Backtrack by seeking to the previous position
-                buf.truncate()
-        if last_left:
-            for i in range(1, left_open + 1):
-                for o in range(i):
-                    buf.write(')')
-                self._rec(buf, target_l, left_open - i, allowed_to_open, res, False)
-                buf.seek(len(buf.getvalue()) - i)  # Backtrack by seeking to the previous position
-                buf.truncate()
+  def generateParenthesis(self, n: int) -> List[str]:
+    to_open = n
+    to_close = n
+    res = []
+    buf = []
+    self._rec(buf, 2 * n, 0, n, res, False)
+    return res
+      
+  def _rec(self, buf, target_l, left_open, allowed_to_open, res, last_left):
+    if len(buf) == target_l:
+      res.append(''.join(buf))
+      return
+    if not last_left:
+      for i in range(1, allowed_to_open + 1):
+        for o in range(i):
+          buf.append('(')
+        self._rec(buf, target_l, left_open + i, allowed_to_open - i, res, True)
+        for o in range(i):
+          buf.pop()
+    if last_left:
+      for i in range(1, left_open + 1):
+        for o in range(i):
+          buf.append(')')
+        self._rec(buf, target_l, left_open - i, allowed_to_open, res, False)
+        for o in range(i):
+          buf.pop()
