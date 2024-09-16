988a96912660694905521463e0f60c947acdb20d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-09-16 02:02:46 +0200

    Time: 34 ms (72.58%), Space: 16.7 MB (48.49%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,42 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res = []
+        i = 0
+        while i < len(words):
+            j = i
+            word_count = 0
+            words_size = 0
+            while j < len(words) and self.min_size(words_size + len(words[j]), word_count + 1) <= maxWidth:
+                words_size += len(words[j])
+                word_count += 1
+                j += 1
+
+            if j == len(words):
+                res.append(" ".join(words[i:j]).ljust(maxWidth))
+            elif word_count == 1:
+                res.append(words[i].ljust(maxWidth))
+            else:
+                number_of_spaces_in_line = maxWidth - words_size
+                number_of_breaks = word_count - 1
+                space_multiplier = floor(number_of_spaces_in_line / number_of_breaks)
+                remainder = number_of_spaces_in_line % number_of_breaks
+                tmp_res = ""
+                for k in range(word_count + number_of_breaks):
+                    if (k % 2) == 0:
+                        tmp_res += words[i + int(k / 2)]
+                    else:
+                        if remainder > 0:
+                            tmp_res += " " * (space_multiplier + 1)
+                            remainder -= 1
+                        else:
+                            tmp_res += " " * space_multiplier
+                res.append(tmp_res)
+            i = j
+        return res
+                    
+
+    def min_size(self, words_length, word_count):
+        return words_length + word_count - 1
+
+
+                 
