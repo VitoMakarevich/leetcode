31c27001fbdd1ba592738298af92cf81f68ba134
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-10 18:35:12 +0200

    Time: 8041 ms (7.67%), Space: 25.5 MB (5.74%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        current_longest = s[0]
+        current_longest_length = 1
+        result_holder = s[0]
+        cache = [[None if i != j else True for j in range(len(s))] for i in range(len(s))]
+        for i in range(len(s)):
+            for j in range(i + 1, len(s)):
+                res = self._internal(s, cache, i, j)
+                if res:
+                    length = j - i + 1
+                    if current_longest_length < length:
+                        current_longest = s[i: j+1]
+                        current_longest_length = length
+        
+        return current_longest
+    
+    def _internal(self, s, cache, i, j):
+        if i == j:
+            return True
+        if j == i + 1:
+            cache[i][j] = s[i] == s[j]
+
+        if cache[i][j] is None:
+            ends_match = s[i] == s[j]
+            inside_match = self._internal(s, cache, i + 1, j - 1)
+            cache[i][j] = ends_match and inside_match
+        return cache[i][j]
+
+        
+        
