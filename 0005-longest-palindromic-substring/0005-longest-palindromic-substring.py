ecfed37bab61afeaf27e72a11e4ed3aafbb78df2
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-10 20:01:14 +0200

    Time: 7648 ms (8.35%), Space: 21 MB (17.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,9 +3,11 @@ class Solution:
         current_longest = s[0]
         current_longest_length = 1
         result_holder = s[0]
-        cache = [[None if i != j else True for j in range(len(s))] for i in range(len(s))]
+        cache = [[None if j!=0 else True for j in range(len(s) - i)] for i in range(len(s))]
+        # print(cache)
         for i in range(len(s)):
             for j in range(i + 1, len(s)):
+                # print(f"starting with i={i}, j={j}")
                 res = self._internal(s, cache, i, j)
                 if res:
                     length = j - i + 1
@@ -15,14 +17,15 @@ class Solution:
         return current_longest
     
     def _internal(self, s, cache, i, j):
-        if cache[i][j] is None:
+        # print(f"i={i}, j={j}")
+        if cache[i][j - i] is None:
             if j == i + 1:
-                cache[i][j] = s[i] == s[j]
+                cache[i][j - i] = s[i] == s[j]
             else:
                 ends_match = s[i] == s[j]
                 inside_match = self._internal(s, cache, i + 1, j - 1)
-                cache[i][j] = ends_match and inside_match
-        return cache[i][j]
+                cache[i][j - i] = ends_match and inside_match
+        return cache[i][j - i]
 
         
         
