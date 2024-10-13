8d484833045eab93838f7320a084a5677d829b5d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-13 14:45:58 +0200

    Time: 78 ms (39.15%), Space: 18 MB (9.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self._internal_search(nums, target, 0, len(nums) - 1, True)
-        right = self._internal_search(nums, target, 0, len(nums) - 1, False)
+        self._nums = nums
+        self._target = target
+        left = self._internal_search(0, len(nums) - 1, True)
+        right = self._internal_search(0, len(nums) - 1, False)
         return [left, right]
 
-    def _internal_search(self, nums, target, left, right, find_leftmost):
+    def _internal_search(self, left, right, find_leftmost):
         if left > right:
             return -1
 
         mid = left + floor((right - left) / 2)
-        if nums[mid] > target:
-            return self._internal_search(nums, target, left, mid - 1, find_leftmost)
-        elif nums[mid] < target:
-            return self._internal_search(nums, target, mid + 1, right, find_leftmost)
+        if self._nums[mid] > self._target:
+            return self._internal_search(left, mid - 1, find_leftmost)
+        elif self._nums[mid] < self._target:
+            return self._internal_search(mid + 1, right, find_leftmost)
         else:
+            next_copied = None
             if find_leftmost:
-                another_to_the_left = self._internal_search(nums, target, left, mid - 1, find_leftmost)
-                if another_to_the_left != -1:
-                    return another_to_the_left
-                else:
-                    return mid
+                next_copied = self._internal_search(left, mid - 1, find_leftmost)
             else:
-                another_to_the_right = self._internal_search(nums, target, mid + 1, right, find_leftmost)
-                if another_to_the_right != -1:
-                    return another_to_the_right
-                else:
-                    return mid
+                next_copied = self._internal_search(mid + 1, right, find_leftmost)
+            return next_copied if next_copied != -1 else mid
