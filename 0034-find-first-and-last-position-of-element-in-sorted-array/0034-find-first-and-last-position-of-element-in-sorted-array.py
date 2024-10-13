2625927a3f388a97a2c469ad137d47090e117825
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-13 14:59:16 +0200

    Time: 65 ms (96.78%), Space: 18 MB (9.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,48 +4,20 @@ class Solution:
         self._target = target
         return self._search_pure_log(nums, target)
 
-    # def search_with_loop(self, nums, target):
-    #     any_coordinate = self._any_match(0, len(nums) - 1)
-    #     if any_coordinate == -1:
-    #         return [-1, -1]
-    #     else:
-    #         right_border = any_coordinate
-    #         while right_border + 1 < len(nums) and nums[right_border + 1] == target:
-    #             right_border += 1
-    #         left_border = any_coordinate
-    #         while left_border - 1 >= 0 and nums[left_border - 1] == target:
-    #             left_border -= 1
-    #         return [left_border, right_border]
-
-    # def _any_match(self, left, right):
-    #     if left > right:
-    #         return -1
-
-    #     mid = left + floor((right - left) / 2)
-    #     if self._nums[mid] == self._target:
-    #         return mid
-    #     if self._nums[mid] > self._target:
-    #         return self._any_match(left, mid - 1)
-    #     else:
-    #         return self._any_match(mid + 1, right)
-
     def _search_pure_log(self, nums, target):
-        left = self._internal_search(0, len(nums) - 1, True)
-        right = self._internal_search(0, len(nums) - 1, False)
-        return [left, right]
-    def _internal_search(self, left, right, find_leftmost):
+        return self._internal_search(0, len(nums) - 1)
+    def _internal_search(self, left, right):
         if left > right:
-            return -1
+            return [-1, -1]
 
         mid = left + floor((right - left) / 2)
         if self._nums[mid] > self._target:
-            return self._internal_search(left, mid - 1, find_leftmost)
+            return self._internal_search(left, mid - 1)
         elif self._nums[mid] < self._target:
-            return self._internal_search(mid + 1, right, find_leftmost)
+            return self._internal_search(mid + 1, right)
         else:
-            next_copied = None
-            if find_leftmost:
-                next_copied = self._internal_search(left, mid - 1, find_leftmost)
-            else:
-                next_copied = self._internal_search(mid + 1, right, find_leftmost)
-            return next_copied if next_copied != -1 else mid
+            leftmost = min(self._internal_search(left, mid - 1))
+            rightmost = max(self._internal_search(mid + 1, right))
+            left_correct = leftmost if leftmost != -1 else mid
+            right_correct = rightmost if rightmost != -1 else mid
+            return [left_correct, right_correct]
