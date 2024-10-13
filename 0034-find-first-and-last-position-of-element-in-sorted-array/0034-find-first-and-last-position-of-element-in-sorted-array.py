5e13a0defd66effd560dc1f14a9252d60efe6d73
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-13 15:02:14 +0200

    Time: 84 ms (8.3%), Space: 17.8 MB (57.28%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,20 +1,23 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        def search(x):
-            lo, hi = 0, len(nums)           
-            while lo < hi:
-                mid = (lo + hi) // 2
-                if nums[mid] < x:
-                    lo = mid+1
-                else:
-                    hi = mid                    
-            return lo
-        
-        lo = search(target)
-        hi = search(target+1)-1
-        
-        if lo <= hi:
-            return [lo, hi]
-                
-        return [-1, -1]
+        self._nums = nums
+        self._target = target
+        return self._search_pure_log(nums, target)
+
+    def _search_pure_log(self, nums, target):
+        return self._internal_search(0, len(nums) - 1)
+    def _internal_search(self, left, right):
+        if left > right:
+            return [-1, -1]
+
+        mid = left + floor((right - left) / 2)
+        if self._nums[mid] > self._target:
+            return self._internal_search(left, mid - 1)
+        elif self._nums[mid] < self._target:
+            return self._internal_search(mid + 1, right)
+        else:
+            leftmost = min(self._internal_search(left, mid - 1))
+            rightmost = max(self._internal_search(mid + 1, right))
+            left_correct = leftmost if leftmost != -1 else mid
+            right_correct = rightmost if rightmost != -1 else mid
+            return [left_correct, right_correct]
