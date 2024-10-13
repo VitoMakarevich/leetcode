a6448a8d9fc526bdf0d91e4b8b6ed1de58fd36e8
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-13 13:08:30 +0200

    Time: 81 ms (83.6%), Space: 24.9 MB (7.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,15 +1,19 @@
 class Solution:
-    @lru_cache(None)
-    def minDistance(self, word1: str, word2: str) -> int:
-        if len(word1) == 0:
-            return len(word2)
-        if len(word2) == 0:
-            return len(word1)
 
-        if word1[-1] == word2[-1]:
-            return self.minDistance(word1[:-1], word2[:-1])
+    def minDistance(self, word1: str, word2: str) -> int:
+        return self._minDistanceMemoryOptimized(word1, word2, len(word1) - 1, len(word2) - 1)
+
+    @lru_cache(None)
+    def _minDistanceMemoryOptimized(self, word1, word2, i, j):
+        if i < 0:
+            return j + 1
+        if j < 0:
+            return i + 1
+
+        if word1[i] == word2[j]:
+            return self._minDistanceMemoryOptimized(word1, word2, i - 1, j - 1)
         else:
-            edit_distance = self.minDistance(word1[:-1], word2[:-1])
-            delete_distance = self.minDistance(word1[:-1], word2)
-            add_distance = self.minDistance(word1, word2[:-1])
+            edit_distance = self._minDistanceMemoryOptimized(word1, word2, i - 1, j - 1)
+            delete_distance = self._minDistanceMemoryOptimized(word1, word2, i - 1, j)
+            add_distance = self._minDistanceMemoryOptimized(word1, word2, i, j - 1)
             return 1 + min(edit_distance, delete_distance, add_distance)
