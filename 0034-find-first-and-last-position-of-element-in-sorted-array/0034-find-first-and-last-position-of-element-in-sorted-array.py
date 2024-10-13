83683803bb5515b1dcff4f50621a12ac1e62ddeb
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-13 14:54:18 +0200

    Time: 82 ms (15.94%), Space: 18 MB (9.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -29,23 +29,23 @@ class Solution:
         else:
             return self._any_match(mid + 1, right)
 
-    def _search_pure_log(self, nums, target):
-        left = self._internal_search(0, len(nums) - 1, True)
-        right = self._internal_search(0, len(nums) - 1, False)
-        return [left, right]
-    def _internal_search(self, left, right, find_leftmost):
-        if left > right:
-            return -1
+    # def _search_pure_log(self, nums, target):
+    #     left = self._internal_search(0, len(nums) - 1, True)
+    #     right = self._internal_search(0, len(nums) - 1, False)
+    #     return [left, right]
+    # def _internal_search(self, left, right, find_leftmost):
+    #     if left > right:
+    #         return -1
 
-        mid = left + floor((right - left) / 2)
-        if self._nums[mid] > self._target:
-            return self._internal_search(left, mid - 1, find_leftmost)
-        elif self._nums[mid] < self._target:
-            return self._internal_search(mid + 1, right, find_leftmost)
-        else:
-            next_copied = None
-            if find_leftmost:
-                next_copied = self._internal_search(left, mid - 1, find_leftmost)
-            else:
-                next_copied = self._internal_search(mid + 1, right, find_leftmost)
-            return next_copied if next_copied != -1 else mid
+    #     mid = left + floor((right - left) / 2)
+    #     if self._nums[mid] > self._target:
+    #         return self._internal_search(left, mid - 1, find_leftmost)
+    #     elif self._nums[mid] < self._target:
+    #         return self._internal_search(mid + 1, right, find_leftmost)
+    #     else:
+    #         next_copied = None
+    #         if find_leftmost:
+    #             next_copied = self._internal_search(left, mid - 1, find_leftmost)
+    #         else:
+    #             next_copied = self._internal_search(mid + 1, right, find_leftmost)
+    #         return next_copied if next_copied != -1 else mid
