573cccbf8c1b4687812d5ed199e183c6ceb4db60
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-16 15:00:27 +0200

    Time: 49 ms (14.74%), Space: 17 MB (13.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -6,18 +6,13 @@ class Solution:
         while True:
             mid = floor((right - left) / 2) + left
             if nums[left] <= nums[mid] and nums[mid] <= nums[right]:
-                # print(f"found result at left={left}, mid={mid}, right={right}, res={right}")
                 res = right
                 break
             elif nums[left] > nums[right] and not nums[mid] > nums[left]:
-                # print(f"moving to left at left={left}, mid={mid}, right={right}, new_right={mid - 1}")
                 right = mid
 
             else:
-                # print(f"moving to right at left={left}, mid={mid}, right={right}, new_right={mid + 1}")
                 left = mid
-        # print(res)
-        # return res
         if res == len(nums) - 1:
             return nums[0]
         else:
