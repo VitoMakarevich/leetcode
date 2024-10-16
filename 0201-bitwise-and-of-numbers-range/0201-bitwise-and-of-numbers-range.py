1593a4e1d296c85a9a0cb19bd164de27ad59d80d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-16 20:18:59 +0200

    Time: 60 ms (20.61%), Space: 16.6 MB (17.24%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,24 +1,23 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        counter = 0
+        leftmost_same_bit = -1
         cp_left = left
         cp_right = right
+        # find leftmost 1 in the same position
+        # if there is no such - there will be no 1 bits at all
         while cp_left > 0 and cp_right > 0:
             cp_left >>= 1
             cp_right >>= 1
-            counter += 1
+            leftmost_same_bit += 1
 
-        leftmost_same_bit = counter - 1
-        # print(f"leftmost_same_bit={leftmost_same_bit},cp_left={bin(cp_left)}, cp_right={bin(cp_right)}")
-        # if leftmost_same_bit == 0 and cp_left == 0 and cp_right == 0:
-        #     return 1 & left & right
-        # elif (cp_left != 0 or cp_right != 0):
-        #     return 0
+        # if there is no 1 bits at all(so no while iterations)
+        # or numbers have 1s at different position - eventually AND will result in 0
         if (leftmost_same_bit == -1) or cp_left != cp_right:
             return 0
         cp_left = left
         cp_right = right
         rightmost_same_bit = leftmost_same_bit
+        # go from leftmost_same bit and get rightmost same bit(can be 0 or 1, just by the same)
         while rightmost_same_bit -1 >= 0:
             temp = rightmost_same_bit - 1
             mask = 1 << temp
