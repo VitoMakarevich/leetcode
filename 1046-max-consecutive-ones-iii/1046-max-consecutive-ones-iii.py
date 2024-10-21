9a32543a6c0f24a51375ce25590f553a845d0696
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-22 00:27:26 +0200

    Time: 75 ms (96.77%), Space: 17.4 MB (6.93%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -5,16 +5,12 @@ class Solution:
         right = 0
         res = float('-inf')
         while right < len(nums):
-            cur = nums[right]
-            if k_zeroes < k:
-                if cur == 0:
-                    k_zeroes += 1
-            else:
-                is_right_zero = nums[right] == 0
-                if is_right_zero:
-                    while nums[left] != 0:
-                        left += 1
+            if k_zeroes < k and nums[right] == 0:
+                k_zeroes += 1
+            elif nums[right] == 0:
+                while nums[left] != 0:
                     left += 1
+                left += 1
             res = max(res, right - left + 1)
             right += 1
 
