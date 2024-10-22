9966556d074e3fe1c8dbfdb020d8039be495dafc
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-22 19:37:58 +0200

    Time: 379 ms (5.01%), Space: 17.2 MB (96.41%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -11,16 +11,19 @@ class Solution:
             if i == len(new):
                 new = ''
             else:
-                j = i
-                current_s = set()
-                while j < len(new) and new[i] not in current_s:
-                    current_s.add(new[j])
-                    j += 1
-                if j - i > i:
-                    if j != len(new):
-                        new = new[0:i] + new[j:]
+                if len(new) - i > i:
+                    j = i
+                    current_s = set()
+                    while j < len(new) and new[i] not in current_s:
+                        current_s.add(new[j])
+                        j += 1
+                    if j - i > i:
+                        if j != len(new):
+                            new = new[0:i] + new[j:]
+                        else:
+                            new = new[0:i]
                     else:
-                        new = new[0:i]
+                        new = new[i:]
                 else:
                     new = new[i:]
             counter += 1
