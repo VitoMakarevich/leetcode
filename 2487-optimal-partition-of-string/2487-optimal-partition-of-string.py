4cb6a3c123afac031f0b30956d23825ed5433aa6
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-22 19:28:56 +0200

    Time: 363 ms (5.01%), Space: 17.3 MB (94.29%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -1,6 +1,29 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        counter = {}
-        for c in s:
-            counter[c] = counter.get(c, 0) + 1
-        return max(counter.values())
+        new = s
+        counter = 0
+        while len(new) > 0:
+            i = 0
+            current_s = set()
+            while i < len(new) and new[i] not in current_s:
+                current_s.add(new[i])
+                i += 1
+            if i == len(new):
+                new = ''
+            else:
+                j = i
+                current_s = set()
+                while j < len(new) and new[i] not in current_s:
+                    current_s.add(new[j])
+                    j += 1
+                if j - i > i:
+                    if j != len(new):
+                        new = new[0:i] + new[j:]
+                    else:
+                        new = new[0:i]
+                else:
+                    new = new[i:]
+            counter += 1
+        
+        return counter
+        
