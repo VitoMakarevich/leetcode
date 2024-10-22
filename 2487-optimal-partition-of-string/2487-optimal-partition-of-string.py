f2f2d23ebd83ef97c2b18d1e2bf88e14b9c943c2
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-22 19:46:31 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -2,28 +2,31 @@ class Solution:
     def partitionString(self, s: str) -> int:
         new = s
         counter = 0
+        i = 0
+        left_set = set()
         while len(new) > 0:
-            i = 0
-            current_s = set()
-            while i < len(new) and new[i] not in current_s:
-                current_s.add(new[i])
+            while i < len(new) and new[i] not in left_set:
+                left_set.add(new[i])
                 i += 1
             if i == len(new):
                 new = ''
             else:
                 if len(new) - i > i:
                     j = i
-                    current_s = set()
-                    while j < len(new) and new[i] not in current_s:
-                        current_s.add(new[j])
+                    right_set = set()
+                    while j < len(new) and new[i] not in right_set:
+                        right_set.add(new[j])
                         j += 1
-                    if j - i > i:
+                    j_for_compare = j - int(j == len(new))
+                    if j_for_compare - i > i:
                         if j != len(new):
                             new = new[0:i] + new[j:]
                         else:
                             new = new[0:i]
                     else:
                         new = new[i:]
+                        i = 0
+                        left_set = set()
                 else:
                     new = new[i:]
             counter += 1
