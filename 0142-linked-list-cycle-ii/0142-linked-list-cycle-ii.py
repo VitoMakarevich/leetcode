3b1e248f16920c430eedb7de43bf3a6742843343
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-25 14:12:57 +0200

    Time: 39 ms (81.57%), Space: 19.2 MB (20.54%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,20 +7,22 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow = head
-        fast = head.next.next if head and head.next else None
-        fast_prev = head
-        left_visited = set()
+        fast = head
+        
         while slow and fast:
-            left_visited.add(slow)
-            if fast in left_visited:
-                break
             slow = slow.next
-            fast_prev = fast
             fast = fast.next.next if fast.next else None
-            
+            if fast == slow:
+                break
         if slow is None or fast is None:
             return None
-        if fast_prev.next in left_visited:
-            return fast_prev.next
-        return fast
+        meet_point = slow
+        
+        left = head
+        right = meet_point
+        while left != right:
+            left = left.next
+            right = right.next
+
+        return left
     
