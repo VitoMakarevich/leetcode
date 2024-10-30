fe2f53dad36b118f774cfe8e4e0484b0a0228696
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-30 20:19:35 +0100

    Time: 1288 ms (16.28%), Space: 17.7 MB (42.58%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.py
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.py
@@ -9,11 +9,8 @@ class Solution:
         if pos >= len(prices):
             return 0
         if pos not in cache:
-            max_allowed_gap_to_skip = pos + 1
-            min_required_to_buy = max_allowed_gap_to_skip + 1
-            range_adjustment = min_required_to_buy + 1
             # print(f"for pos {pos} max_allowed_gap_to_skip is {max_allowed_gap_to_skip}, min_required_to_buy={min_required_to_buy}, range_adjustment={range_adjustment}")
-            r = range(1, range_adjustment)
+            r = range(1, pos + 3)
 
             res = prices[pos] + min([
                 self._dp(cache, pos + i, prices) for i in r
