e6438862888e2dc86631a157e26606ebfd59204f
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-31 16:46:25 +0100

    Time: 526 ms (13.78%), Space: 29.7 MB (18.92%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -8,8 +8,12 @@ class Solution:
             if nums_with_counts[v] > 0:
                 nums_with_counts[v] -= 1
                 if nums_with_counts.get(k - v, 0) > 0:
-                    res += 1
                     nums_with_counts[k - v] -= 1
+                    res += 1
+                    while k - v != v and nums_with_counts[v] > 0 and nums_with_counts[k - v] > 0:
+                        res += 1
+                        nums_with_counts[v] -= 1
+                        nums_with_counts[k - v] -= 1
             else:
                 nums_with_counts[v] -= 1
         return res
