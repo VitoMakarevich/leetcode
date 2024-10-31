8ef0f49c06e66912e8ad95cf6028c73caefcb896
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-10-31 16:55:04 +0100

    Time: 472 ms (96.09%), Space: 29.7 MB (18.92%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -4,11 +4,11 @@ class Solution:
         res = 0
         used = set()
         for item, amount in nums_with_counts.items():
-            if item not in used:
+            if amount > 0:
                 if k - item == item:
                     res += floor(amount / 2)
                 elif nums_with_counts.get(k - item, 0) > 0:
                     res += min(amount, nums_with_counts[k - item])
-                    used.add(k - item)
+                    nums_with_counts[k - item] = 0
 
         return res
