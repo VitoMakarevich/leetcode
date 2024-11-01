89d25ea47b9a25312fe4a96b6b0c99a4c9bca467
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-01 14:16:32 +0100

    Time: 15 ms (49.67%), Space: 16.7 MB (90.06%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,34 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        voted = deque()
+        not_voted = deque()
+        for vote in senate:
+            if not_voted and not_voted[-1] != vote:
+
+                voted.append(not_voted.pop())
+            else:
+                not_voted.append(vote)
+         
+        new_iter_set = set(voted + not_voted)
+        iterations = not_voted + voted
+        # print(voted, not_voted, new_iter_set)
+        while len(new_iter_set) != 1:
+            voted = deque()
+            not_voted = deque()
+            for vote in iterations:
+                if not_voted and not_voted[-1] != vote:
+                    voted.append(not_voted.pop())
+                else:
+                    not_voted.append(vote)
+            # print(voted, not_voted)
+            iterations = not_voted + voted
+            new_iter_set = set(voted + not_voted)
+        #     print(voted, not_voted, new_iter_set)
+        
+        return self.fmt(next(iter(new_iter_set)))
+            
+    def fmt(self, v):
+        if v == 'R':
+            return 'Radiant'
+        else:
+            return 'Dire'
