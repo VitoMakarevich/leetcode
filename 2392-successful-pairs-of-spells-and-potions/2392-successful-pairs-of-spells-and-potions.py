13c6404631ce1eb61067e7d8383da31f2eb697e3
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-06 13:49:58 +0100

    Time: 143 ms (89.19%), Space: 38.1 MB (77.77%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -1,14 +1,12 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         potions.sort()
-        cache = {}
         res = []
         for s in spells:
-            if not s in cache:
-                min_multiplier = ceil(success/s)
-                pos = bisect.bisect_left(potions, min_multiplier)
-                count = len(potions) - pos
-                cache[s] = count
-            res.append(cache[s])
+            min_multiplier = ceil(success/s)
+            pos = bisect.bisect_left(potions, min_multiplier)
+            count = len(potions) - pos
+            
+            res.append(count)
 
         return res
