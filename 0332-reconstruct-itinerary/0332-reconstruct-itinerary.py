6dc86445c8d0e1e5d41e8ce10709cf0aee0f53bb
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-12 19:47:00 +0100

    Time: 4 ms (45.29%), Space: 16.9 MB (76.33%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,49 +1,22 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        available_tickets = {}
         graph = {}
-        path = deque()
-        path.append('JFK')
-        for t in tickets:
-            ticket = (t[0], t[1])
-            available_tickets[ticket] = available_tickets.get(ticket, 0) + 1
-            source, target = t
-            if source not in graph:
-                graph[source] = []
-            graph[source].append(target)
+        for src, dst in tickets:
+            if src in graph:
+                graph[src].append(dst)
+            else:
+                graph[src] = [dst]
+
+        for src in graph.keys():
+            graph[src].sort(reverse=True)
+        stack = []
         res = []
-        self._dfs(available_tickets, graph, len(tickets) + 1, res, 'JFK', path)
-        return res[0]
-    def _dfs(self, available_tickets, graph, target_tickets, res, node, path):
-
-        if len(path) == target_tickets:
-            if len(res) == 0:
-                res.append(list(path))
+        stack.append("JFK")
+  
+        while len(stack) > 0:
+            elem = stack[-1]
+            if elem in graph and len(graph[elem]) > 0: 
+                stack.append(graph[elem].pop())
             else:
-                res[0] = self._compare_paths(res[0], path)
-
-        if node in graph:
-            for adj in graph[node]:
-                ticket = (node, adj)
-                if available_tickets.get(ticket, 0) > 0:
-                    available_tickets[ticket] -= 1
-                    path.append(adj)
-                    self._dfs(available_tickets, graph, target_tickets, res, adj, path)
-                    available_tickets[ticket] += 1
-                    path.pop()
-
-
-    def _compare_paths(self, existing, candidate):
-        lst_candidate = list(candidate)
-        cur_word = 0
-        cur_pos_in_word = 0
-        while existing[cur_word][cur_pos_in_word] == lst_candidate[cur_word][cur_pos_in_word] and cur_word < len(existing) - 1:
-            if cur_pos_in_word < 2:
-                cur_pos_in_word += 1
-            else:
-                cur_word += 1
-                cur_pos_in_word = 0
-        if existing[cur_word][cur_pos_in_word] < lst_candidate[cur_word][cur_pos_in_word]:
-            return existing
-        else:
-            return lst_candidate
+                res.append(stack.pop())
+        return res[::-1]
