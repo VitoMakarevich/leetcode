28073abfd062ab937cfb14b78a7bb61df8a126c5
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-13 19:32:42 +0100

    Time: 75 ms (11.9%), Space: 18.3 MB (17.21%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -26,29 +26,59 @@ class Solution:
                 if len(number):
                     numbers.append(int(''.join(number)))
                     number = deque()
-                operations_to_be_done = deque()
-                numbers_to_be_used = deque()
-                while len(operators) and operators[-1] != '(':
-                    operations_to_be_done.appendleft(operators.pop())
-                while len(numbers) and numbers[-1] != '(':
-                    numbers_to_be_used.appendleft(numbers.pop())
-                if len(operations_to_be_done) == len(numbers_to_be_used):
-                    numbers_to_be_used.appendleft(-numbers_to_be_used.popleft())
-                    operations_to_be_done.popleft()
-                while len(operations_to_be_done):
-                    op = operations_to_be_done.popleft()
-                    left_operand = numbers_to_be_used.popleft()
-                    right_operand = numbers_to_be_used.popleft()
-                    if op == '-':
-                        numbers_to_be_used.appendleft(left_operand - right_operand)
+                # print(f"op={operators}, numbers={numbers}")
+                while operators[-1] != '(':
+                    # print(f"op={operators}, numbers={numbers}")
+                    right_operand = numbers.pop()
+                    op = operators.pop()
+                    if numbers[-1] == '(':
+                        if op == '-':
+                            # print(f"inverting sign of {right_operand}")
+                            numbers.append(-right_operand)
+                        else:
+                            numbers.append(right_operand)
                     else:
-                        numbers_to_be_used.appendleft(left_operand + right_operand)
-                # remove (
+                        left_operand = numbers.pop()
+                        if operators[-1] != '(':
+                            left_operand_sign = operators[-1]
+                            if left_operand_sign == '-':
+                                left_operand = -left_operand
+                                operators.pop()
+                                operators.append('+')
+                        if op == '-':
+                            numbers.append(left_operand - right_operand)
+                        else:
+                            numbers.append(left_operand + right_operand)
+                # print(f"e op={operators}, numbers={numbers}")
                 operators.pop()
+                last_n = numbers.pop()
                 numbers.pop()
-                #
+                numbers.append(last_n)
+                # print(f"end, op={operators}, numbers={numbers}")
+                # operations_to_be_done = deque()
+                # numbers_to_be_used = deque()
+                # while len(operators) and operators[-1] != '(':
+                #     operations_to_be_done.appendleft(operators.pop())
+                # while len(numbers) and numbers[-1] != '(':
+                #     numbers_to_be_used.appendleft(numbers.pop())
+                # if len(operations_to_be_done) == len(numbers_to_be_used):
+                #     numbers_to_be_used.appendleft(-numbers_to_be_used.popleft())
+                #     operations_to_be_done.popleft()
+                # while len(operations_to_be_done):
+                #     op = operations_to_be_done.popleft()
+                #     left_operand = numbers_to_be_used.popleft()
+                #     right_operand = numbers_to_be_used.popleft()
+                #     if op == '-':
+                #         numbers_to_be_used.appendleft(left_operand - right_operand)
+                #     else:
+                #         numbers_to_be_used.appendleft(left_operand + right_operand)
+                # # remove (
+                # operators.pop()
+                # numbers.pop()
+                # #
 
-                numbers.append(numbers_to_be_used[0])
+                # numbers.append(numbers_to_be_used[0])
             else:
                 number.append(c)
+        # print(numbers, operators)
         return numbers[0]
