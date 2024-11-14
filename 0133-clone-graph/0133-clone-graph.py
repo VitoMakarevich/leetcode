5a306ba99292d31ffad15a11e027006d50270e9e
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-14 18:40:27 +0100

    Time: 50 ms (7.87%), Space: 17 MB (63.78%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,24 +11,14 @@ class Solution:
     nodes = []
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         self.nodes = {}
-        self.visited = set()
         if node is None:
           return None
         res = Node(node.val)
         self.nodes[node.val] = res
         self.dig(node, res, None)
 
-        # vis = set()
-        # self.log(res, vis)
         return res
         
-    def log(self, root, vis):
-      vis.add(root.val)
-      print(vis)
-      print(f"for node {root.val} neighbours are {list(map(lambda x: x.val, root.neighbors))}")
-      for neighbor in [n for n in root.neighbors if not n.val in vis]:
-        self.log(neighbor, vis)
-
     def dig(self, old_node, new_node, prev):
       for old_neighbor in old_node.neighbors:
         if not old_neighbor.val in self.nodes :
@@ -39,7 +29,6 @@ class Solution:
             
         elif not prev or (prev and old_neighbor.val != prev.val):
             new_node.neighbors.append(self.nodes[old_neighbor.val])
-            # print(f"adding neighbor {old_neighbor.val} into {new_node.val} at {new_node.val}")
             
 
 
