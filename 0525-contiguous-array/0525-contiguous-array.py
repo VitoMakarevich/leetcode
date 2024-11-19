5c0621f047c23c9613c69de6f68ec824aaf0e666
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-19 18:21:13 +0100

    Time: 127 ms (14.61%), Space: 26.2 MB (5.01%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -2,15 +2,17 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         prefix_sum = []
         for value in nums:
-            if len(prefix_sum) == 0:
-                prefix_sum.append(value if value == 1 else -1)
-            else:
-                v = value if value == 1 else -1
-                prefix_sum.append(prefix_sum[-1] + v)
+            prev = 0 if len(prefix_sum) == 0 else prefix_sum[-1]
+            
+            v = value if value == 1 else -1
+            prefix_sum.append(prev + v)
         counter = {0: [-1]}
         for k, v in enumerate(prefix_sum):
             cur_v = counter.get(v, [])
-            cur_v.append(k)
+            if len(cur_v) == 0 or len(cur_v) == 1:
+                cur_v.append(k)
+            else:
+                cur_v[1] = k
             counter[v] = cur_v
         res = 0
         for values in counter.values():
