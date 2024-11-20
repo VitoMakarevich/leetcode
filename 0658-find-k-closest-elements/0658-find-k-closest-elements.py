60a2d342a5bda49017db9a8b9b7432510d2eeccd
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-20 17:56:41 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # 2 solutions possible:
+        # 1. with 2 heaps - time complexity O((n + k) logK), memory - O(k)
+        # 2. with binary search - logN + k, memory - O(1)
+        # bisect is clear winner
+        res = deque()
+        left = 0
+        right = len(arr)
+        while left < right:
+            mid = floor((right - left)/2 + left)
+            if x < arr[mid]:
+                right = mid
+            elif x > arr[mid]:
+                left = mid + 1
+            else:
+                left = mid - 1
+                break
+        left = left - 1
+        right = left + 1
+        while len(res) < k:
+            if left >= 0 and right < len(arr):
+                left_diff = abs(arr[left] - x)
+                right_diff = abs(arr[right] - x)
+                if left_diff <= right_diff:
+                    res.appendleft(arr[left])
+                    left -= 1
+                else:
+                    res.append(arr[right])
+                    right += 1
+            elif left < 0:
+                res.append(arr[right])
+                right += 1
+            else:
+                res.appendleft(arr[left])
+                left -= 1
+        return list(res)
+
+
+        
