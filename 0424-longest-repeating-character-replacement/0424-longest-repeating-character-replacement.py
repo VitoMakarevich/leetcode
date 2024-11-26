1a0ca3f8809d21450f94ce4a96f806dfc121d156
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-26 18:13:23 +0100

    Time: 128 ms (17.13%), Space: 17.9 MB (9.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,12 +5,6 @@ class Solution:
         res = 0
         elements = {}
         for v in s:
-            if len(elements) == 0:
-                elements[v] = 1
-                q.append(v)
-                size += 1
-                res = max(res, size)
-                continue
             size += 1
             elements[v] = elements.get(v, 0) + 1
             max_in_elements = max(elements.values())
