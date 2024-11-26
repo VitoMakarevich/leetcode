04c5ad5371bfd541f1641efa589474c66bdab440
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-26 18:09:17 +0100

    Time: 172 ms (12.21%), Space: 18 MB (9.59%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        size = 0
+        q = deque()
+        res = 0
+        elements = {}
+        for v in s:
+            if len(elements) == 0:
+                elements[v] = 1
+                q.append(v)
+                size += 1
+                res = max(res, size)
+                continue
+            size += 1
+            elements[v] = elements.get(v, 0) + 1
+            max_in_elements = max(elements.values())
+            sum_elements = sum(elements.values())
+            if sum_elements - max_in_elements > k:
+                size -= 1
+                elements[q.popleft()] -= 1
+            q.append(v)
+            res = max(res, size)
+        return res
