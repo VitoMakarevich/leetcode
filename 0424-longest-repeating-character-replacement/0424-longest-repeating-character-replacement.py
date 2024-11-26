c3dfe6aac8f6252510d3b8ec6c653737dbc1b878
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-26 18:13:46 +0100

    Time: 108 ms (22.8%), Space: 17.8 MB (9.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,13 +4,17 @@ class Solution:
         q = deque()
         res = 0
         elements = {}
+        max_in_elements = 0
         for v in s:
             size += 1
             elements[v] = elements.get(v, 0) + 1
-            max_in_elements = max(elements.values())
+            max_in_elements = max(elements[v], max_in_elements)
             if size - max_in_elements > k:
                 size -= 1
-                elements[q.popleft()] -= 1
+                f = q.popleft()
+                elements[f] -= 1
+                if v == f:
+                    max_in_elements -= 1
             q.append(v)
             res = max(res, size)
         return res
