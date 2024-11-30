d4dc37e26ead68d318884ad0b0772b58e9b87e45
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-30 18:20:48 +0100

    Time: 88 ms (46.24%), Space: 23.6 MB (6.76%) - LeetHub

--- 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -9,22 +9,18 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
         seek_nodes = set()
         for node in nodes:
-          seek_nodes.add(node.val)
+          seek_nodes.add(node)
         cur = root
         p = []
         res_path = []
-        found = [False]
-        self._dfs(p, res_path, seek_nodes, root, found)
-
+        self._dfs(p, res_path, seek_nodes, root)
         return TreeNode(res_path[0][-1])
         
 
-    def _dfs(self, cur_path, res_path, seek_nodes, cur_node, found):
-      if found[0] == True:
-        return
+    def _dfs(self, cur_path, res_path, seek_nodes, cur_node):
       cur_path.append(cur_node.val)
-      if cur_node.val in seek_nodes:
-        seek_nodes.remove(cur_node.val)
+      if cur_node in seek_nodes:
+        seek_nodes.remove(cur_node)
         if len(res_path) == 0:
           res_path.append(cur_path.copy())
         else:
@@ -32,11 +28,8 @@ class Solution:
           while i < len(res_path[0]) and i < len(cur_path) and cur_path[i] == res_path[0][i]:
             i += 1
           res_path[0] = res_path[0][0: i]
-        if len(seek_nodes) == 0:
-          found[0] = True
-          return 
       if cur_node.left:
-        self._dfs(cur_path, res_path, seek_nodes, cur_node.left, found)
+        self._dfs(cur_path, res_path, seek_nodes, cur_node.left)
       if cur_node.right:
-        self._dfs(cur_path, res_path, seek_nodes, cur_node.right, found)
+        self._dfs(cur_path, res_path, seek_nodes, cur_node.right)
       cur_path.pop()
