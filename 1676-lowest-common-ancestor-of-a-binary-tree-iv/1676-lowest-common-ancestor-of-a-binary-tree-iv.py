7b332a9c8e9cb1f6a52f77dac5601951a9dbbe9c
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-30 17:59:39 +0100

    Time: 876 ms (8.03%), Space: 311.1 MB (5.05%) - LeetHub

---
+++ 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        seek_nodes = set()
+        for node in nodes:
+          seek_nodes.add(node.val)
+        cur = root
+        p = deque()
+        paths = deque()
+        self._dfs(p, paths, seek_nodes, root)
+        last_popped = None
+        while min(map(len, paths)) > 0 and len(set(map(lambda x: x[0].val, paths))) == 1:
+          for local_path in paths:
+            last_popped = local_path.popleft()
+        
+        return last_popped
+        
+
+    def _dfs(self, cur_path, paths, seek_nodes, cur_node):
+      cur_path.append(cur_node)
+      if cur_node.val in seek_nodes:
+        paths.append(cur_path.copy())
+      if len(paths) == len(seek_nodes):
+        return
+      if cur_node.left:
+        self._dfs(cur_path, paths, seek_nodes, cur_node.left)
+      if cur_node.right:
+        self._dfs(cur_path, paths, seek_nodes, cur_node.right)
+      cur_path.pop()
+
+    def _debug(self, paths):
+      out = []
+      for p in paths:
+        local = []
+        for item in p:
+          local.append(item.val)
+        out.append(local)
+      print(out)
