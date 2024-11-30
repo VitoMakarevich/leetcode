d15bfe0ff146377ada9f79dfc7a8534a157749f7
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-11-30 18:14:34 +0100

    Time: 101 ms (21.37%), Space: 23.6 MB (7.1%) - LeetHub

--- 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.py
+++ 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -11,10 +11,9 @@ class Solution:
         for node in nodes:
           seek_nodes.add(node.val)
         cur = root
-        p = deque()
+        p = []
         res_path = []
         self._dfs(p, res_path, seek_nodes, root)
-        # print(res_path)
         return res_path[0][-1]
         
 
@@ -25,12 +24,10 @@ class Solution:
         if len(res_path) == 0:
           res_path.append(cur_path.copy())
         else:
-          cur_copy = cur_path.copy()
-          new_res = deque()
-          while len(res_path[0]) > 0 and len(cur_copy) > 0 and res_path[0][0].val == cur_copy[0].val:
-            new_res.append(cur_copy.popleft())
-            res_path[0].popleft()
-          res_path[0] = new_res
+          i = 0
+          while i < len(res_path[0]) and i < len(cur_path) and cur_path[i].val == res_path[0][i].val:
+            i += 1
+          res_path[0] = res_path[0][0: i]
       if len(seek_nodes) == 0:
         return
       if cur_node.left:
