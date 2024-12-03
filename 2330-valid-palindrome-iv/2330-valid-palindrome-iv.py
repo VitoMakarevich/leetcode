999b140bf3e15b12244a65a34d5b6fb7b394c25c
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-03 18:20:06 +0100

    Time: 15 ms (6.47%), Space: 17.5 MB (10.15%) - LeetHub

---
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
@@ -0,0 +1,15 @@
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+      if len(s) == 1:
+        return True
+      center = len(s) // 2
+      left = center if len(s) % 2 == 1 else center - 1
+      right = center
+      mismatch = 0
+      while left >= 0 and right < len(s):
+        mismatch += s[left] != s[right]
+        # print(f"l = {left}, r = {right}, mismatch = {mismatch}")
+        left -= 1
+        right += 1
+      # print(mismatch)
+      return mismatch < 3
