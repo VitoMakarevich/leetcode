e58fe0a66b01af17361f99de069c585d2c39e3ec
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-12 11:00:18 +0100

    Time: 3 ms (45.9%), Space: 17.7 MB (5.53%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -4,13 +4,15 @@ class Solution:
 
         push_i = 0
         pop_i = 0
-        while push_i < len(pushed) or pop_i < len(popped):
-          if len(s) > 0 and pop_i < len(popped) and s[-1] == popped[pop_i]:
+        l_pushed = len(pushed)
+        l_popped = len(popped)
+        while push_i < l_pushed or pop_i < l_popped:
+          if len(s) > 0 and pop_i < l_popped and s[-1] == popped[pop_i]:
             s.pop()
             pop_i += 1
-          elif len(pushed) > 0 and (len(s) == 0 or push_i < len(pushed)):
+          elif l_pushed > 0 and (len(s) == 0 or push_i < l_pushed):
             s.append(pushed[push_i])
             push_i += 1
           else:
             return False
-        return push_i == len(pushed) and pop_i == len(popped) 
+        return push_i == l_pushed and pop_i == l_popped
