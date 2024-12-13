1e0dc4f7ffb3749691ccedcf569a201cb6bca985
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-13 16:02:07 +0100

    Time: 141 ms (27.66%), Space: 55.4 MB (8.39%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -4,24 +4,24 @@ class Solution:
         total_size = len(grid) * len(grid[0])
         visited = set()
         for i in range(len(grid)):
-          self._dfs((i, 0), visited, len(grid), len(grid[0]), grid)
-          self._dfs((i, len(grid[0]) - 1), visited, len(grid), len(grid[0]), grid)
+          if grid[i][0] == 1:
+            self._dfs((i, 0), visited, len(grid), len(grid[0]), grid)
+          if grid[i][len(grid[0]) - 1] == 1:
+            self._dfs((i, len(grid[0]) - 1), visited, len(grid), len(grid[0]), grid)
         for j in range(len(grid[0])):
-          self._dfs((0, j), visited, len(grid), len(grid[0]), grid)
-          self._dfs((len(grid) - 1, j), visited, len(grid), len(grid[0]), grid)
+          if grid[0][j] == 1:
+            self._dfs((0, j), visited, len(grid), len(grid[0]), grid)
+          if grid[len(grid) - 1][j] == 1:
+            self._dfs((len(grid) - 1, j), visited, len(grid), len(grid[0]), grid)
         count_zeros = 0
         for row in grid:
-          for cell in row:
-            if cell == 0:
-              count_zeros += 1
+          count_zeros += len(row) - sum(row)
         return total_size - count_zeros - len(visited)
         
     def _dfs(self, coord, visited, rows, cols, grid):
-      if grid[coord[0]][coord[1]] == 0:
-        return
       visited.add(coord)
       for direction in self.get_valid_directions(coord[0], coord[1], rows, cols):
-        if not direction in visited:
+        if not direction in visited and grid[direction[0]][direction[1]] == 1:
           self._dfs(direction, visited, rows, cols, grid)
 
     def get_valid_directions(self, x, y, rows, cols):
