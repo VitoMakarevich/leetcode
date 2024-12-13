22384c89215b20e8a2f551affaf04518906c1e92
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-13 15:59:10 +0100

    Time: 143 ms (27.06%), Space: 55.5 MB (8.39%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        self.directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        total_size = len(grid) * len(grid[0])
+        visited = set()
+        for i in range(len(grid)):
+          self._dfs((i, 0), visited, len(grid), len(grid[0]), grid)
+          self._dfs((i, len(grid[0]) - 1), visited, len(grid), len(grid[0]), grid)
+        for j in range(len(grid[0])):
+          self._dfs((0, j), visited, len(grid), len(grid[0]), grid)
+          self._dfs((len(grid) - 1, j), visited, len(grid), len(grid[0]), grid)
+        count_zeros = 0
+        for row in grid:
+          for cell in row:
+            if cell == 0:
+              count_zeros += 1
+        return total_size - count_zeros - len(visited)
+        
+    def _dfs(self, coord, visited, rows, cols, grid):
+      if grid[coord[0]][coord[1]] == 0:
+        return
+      visited.add(coord)
+      for direction in self.get_valid_directions(coord[0], coord[1], rows, cols):
+        if not direction in visited:
+          self._dfs(direction, visited, rows, cols, grid)
+
+    def get_valid_directions(self, x, y, rows, cols):
+      valid_directions = []
+      
+      for dx, dy in self.directions:
+          nx, ny = x + dx, y + dy
+          if 0 <= nx < rows and 0 <= ny < cols:
+              valid_directions.append((nx, ny))
+      
+      return valid_directions
