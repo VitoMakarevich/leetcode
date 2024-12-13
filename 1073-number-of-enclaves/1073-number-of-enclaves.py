7e7bc96a8ab095b53008ff4ff1d570b3ddd22323
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-13 16:07:18 +0100

    Time: 106 ms (43.26%), Space: 52.4 MB (10.71%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -2,24 +2,23 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         self.directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         total_size = len(grid) * len(grid[0])
-        visited = set()
         for i in range(len(grid)):
           if grid[i][0] == 1:
-            self._dfs((i, 0), visited, len(grid), len(grid[0]), grid)
+            self._dfs((i, 0), len(grid), len(grid[0]), grid)
           if grid[i][len(grid[0]) - 1] == 1:
-            self._dfs((i, len(grid[0]) - 1), visited, len(grid), len(grid[0]), grid)
+            self._dfs((i, len(grid[0]) - 1), len(grid), len(grid[0]), grid)
         for j in range(len(grid[0])):
           if grid[0][j] == 1:
-            self._dfs((0, j), visited, len(grid), len(grid[0]), grid)
+            self._dfs((0, j), len(grid), len(grid[0]), grid)
           if grid[len(grid) - 1][j] == 1:
-            self._dfs((len(grid) - 1, j), visited, len(grid), len(grid[0]), grid)
+            self._dfs((len(grid) - 1, j), len(grid), len(grid[0]), grid)
         return sum(sum(row) for row in grid)
         
-    def _dfs(self, coord, visited, rows, cols, grid):
+    def _dfs(self, coord, rows, cols, grid):
       grid[coord[0]][coord[1]] = 0
       for direction in self.get_valid_directions(coord[0], coord[1], rows, cols):
         if grid[direction[0]][direction[1]] == 1:
-          self._dfs(direction, visited, rows, cols, grid)
+          self._dfs(direction, rows, cols, grid)
 
     def get_valid_directions(self, x, y, rows, cols):
       valid_directions = []
