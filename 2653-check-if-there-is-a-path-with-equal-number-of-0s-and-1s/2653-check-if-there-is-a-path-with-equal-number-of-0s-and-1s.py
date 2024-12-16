1215be9f1a4124202c8256cea52db0c78757e1ad
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-16 20:46:55 +0100

    Time: 787 ms (23.44%), Space: 144.1 MB (15.15%) - LeetHub

---
+++ 2653-check-if-there-is-a-path-with-equal-number-of-0s-and-1s/2653-check-if-there-is-a-path-with-equal-number-of-0s-and-1s.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isThereAPath(self, grid: List[List[int]]) -> bool:
+      self._grid = grid
+      res = self.is_a_path(0, 0, 0)
+      return res
+        
+    @cache
+    def is_a_path(self, x, y, path_sum):
+      cur_price = -1 if self._grid[x][y] == 0 else 1
+      if x == len(self._grid) - 1 and y == len(self._grid[0]) - 1:
+        return path_sum + cur_price == 0
+      else:
+        return (x + 1 < len(self._grid) and self.is_a_path(x + 1, y, path_sum + cur_price)) or (
+          y + 1 < len(self._grid[0]) and self.is_a_path(x, y + 1, path_sum + cur_price)
+        )
