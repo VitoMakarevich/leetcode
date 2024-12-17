c6d0130f9312c33f8367d10384cec7001d629aa4
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-17 23:02:46 +0100

    Time: 3 ms (95.11%), Space: 18.2 MB (6.25%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,16 +1,17 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        cnt = 0
-        local_zeros = 1
-        for v in flowerbed + [0, 1]:
-          if v:
-            if local_zeros >= 3:
-              cnt += 1
-              if local_zeros > 3:
-                cnt += int(floor((local_zeros - 3) / 2))
-            local_zeros = 0
-          else:
-            local_zeros += 1
-          if cnt >= n:
-            return True
-        return False
+        self.cnt = 0
+        self.local_zeros = 1
+        def map_l(l):
+          for v in l:
+            if v:
+              if self.local_zeros >= 3:
+                self.cnt += 1
+                if self.local_zeros > 3:
+                  self.cnt += int(floor((self.local_zeros - 3) / 2))
+              self.local_zeros = 0
+            else:
+              self.local_zeros += 1
+            if self.cnt >= n:
+              return True
+        return map_l(flowerbed) or map_l(deque([0, 1])) or False
