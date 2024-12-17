ba5c4b879d702f2fe946019a7292ffbfaadb0e7d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-17 23:06:00 +0100

    Time: 4 ms (84.68%), Space: 18.1 MB (6.25%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,16 +1,16 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        self.cnt = 0
         self.local_zeros = 1
         def map_l(l):
+          nonlocal n
           for v in l:
             if v:
               if self.local_zeros >= 3:
-                self.cnt += 1
-                self.cnt += max(0, int(floor((self.local_zeros - 3) / 2)))
+                n -= 1
+                n -= max(0, int(floor((self.local_zeros - 3) / 2)))
               self.local_zeros = 0
             else:
               self.local_zeros += 1
-            if self.cnt >= n:
+            if n <= 0:
               return True
         return map_l(flowerbed) or map_l(deque([0, 1])) or False
