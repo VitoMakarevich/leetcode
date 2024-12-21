38aa1c2f3506116b6a24fc37b96b9e2713493630
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-21 11:03:35 +0100

    Time: 40 ms (45.44%), Space: 18 MB (5.55%) - LeetHub

--- 0873-guess-the-word/0873-guess-the-word.py
+++ 0873-guess-the-word/0873-guess-the-word.py
@@ -8,22 +8,64 @@
 class Solution:
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
         options = words.copy()
-        last_option = None
+        last_options = []
         
         while True:
-          if last_option:
-            lo_matches, lo_value = last_option
-            options = list(filter(lambda x: self.position_match(lo_value, x) >= lo_matches, options))
+          if len(last_options):
+            options = list(filter(lambda x: self.accepted(x, last_options), options))
+            popularity = self._calculate_dict_popularity(options)
+            options.sort(key = lambda x: self._word_popularity(x, popularity), reverse=True)
           next_guess = options[0]
           options = options[1:]
-          print(next_guess)
+         
           res = master.guess(next_guess)
+          # if len(last_options):
+          #   print(next_guess, res, self._matches_per_prev(next_guess, last_options), self.number_of_mismatches(next_guess, last_options))
+          # else:
+          #   print(next_guess, res)
           if res == 6:
             return
-          last_option = (res, next_guess)
+          last_options.append((res, next_guess))
+    def _matches_per_prev(self, candidate, prev):
+      res = []
+      for v in prev:
+        res.append(self.position_match(v[1], candidate))
+      return res
 
+    def number_of_mismatches(self, candidate, prev):
+      m = 0
 
-    
+      for v in prev:
+        m += 6 - self.position_match(v[1], candidate)
+      return m
+
+    def _calculate_dict_popularity(self, candidates):
+      res = []
+      for i in range(6):
+        local = {}
+        for word in candidates:
+          local[word[i]] = local.get(word[i], 0) + 1
+        res.append(local)
+      return res
+    def _word_popularity(self, word, popularity_dict):
+      r = 0
+      for idx, c in enumerate(word):
+        r += popularity_dict[idx][c]
+      return r
+
+    def accepted(self, candidate, prev):
+      for v in prev:
+        matches, word = v
+        mismatches = 6 - matches
+        
+        positional_matches = self.position_match(candidate, word)
+        positional_mismatches = 6 - positional_matches
+        # if more than prev match - throw away
+        # if less than prev match - throw away
+        if positional_matches < matches or mismatches > positional_mismatches:
+          return False
+      return True
+
     def position_match(self, left, right):
       m = 0
       for i in range(len(left)):
