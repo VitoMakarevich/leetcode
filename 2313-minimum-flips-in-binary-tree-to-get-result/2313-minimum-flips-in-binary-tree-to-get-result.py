1ebda82055868efdb72b0dea897fa8650536c16a
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-22 16:35:14 +0100

    Time: 344 ms (43.01%), Space: 79.6 MB (19.35%) - LeetHub

--- 2313-minimum-flips-in-binary-tree-to-get-result/2313-minimum-flips-in-binary-tree-to-get-result.py
+++ 2313-minimum-flips-in-binary-tree-to-get-result/2313-minimum-flips-in-binary-tree-to-get-result.py
@@ -6,56 +6,62 @@
 #         self.right = right
 class Solution:
     def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
-        return self._dp(root, result)
+      return self._dp_cached({}, root, result)
 
-    @cache
-    def _dp(self, root, needed_result):
-          if root.val == 0 or root.val == 1:
-            return int(not needed_result == bool(root.val))
-          elif root.val == 2:
-            if needed_result:
-              left_count = self._dp(root.left, needed_result)
-              if left_count == 0:
-                return 0
-              right_count = self._dp(root.right, needed_result)
-              if right_count == 0:
-                return 0
-              return min(left_count, right_count)
-            else:
-              return self._dp(root.left, needed_result) + self._dp(root.right, needed_result)
-          elif root.val == 3:
-            if needed_result:
-              left_count = self._dp(root.left, needed_result)
-              right_count = self._dp(root.right, needed_result)
-              return left_count + right_count
-            else:
-              left_count = self._dp(root.left, needed_result)
-              if left_count == 0:
-                return 0
-              right_count = self._dp(root.right, needed_result)
-              if right_count == 0:
-                return 0
-              return min(left_count, right_count)
-          elif root.val == 4:
-            candidates = []
-            if needed_result:
-              left_count = self._dp(root.left, True) + self._dp(root.right, False)
-              if left_count == 0:
-                return 0
-              right_count = self._dp(root.left, False) + self._dp(root.right, True)
-              if right_count == 0:
-                return 0
-              return min(left_count, right_count)
-            else:
-              left_count = self._dp(root.left, False) + self._dp(root.right, False)
-              if left_count == 0:
-                return 0
-              right_count = self._dp(root.left, True) + self._dp(root.right, True)
-              if right_count == 0:
-                return 0
-              return min(left_count, right_count)
-          else:
-            if root.left:
-              return self._dp(root.left, not needed_result)
-            else:
-              return self._dp(root.right, not needed_result)
+    def _dp_cached(self, cache, root, needed_result):
+      if not root in cache:
+        cache[root] = {}
+      if not needed_result in cache[root]:
+        cache[root][needed_result] = self._dp(cache, root, needed_result)
+      return cache[root][needed_result]
+
+    def _dp(self, cache, root, needed_result):
+      if root.val == 0 or root.val == 1:
+        return int(not needed_result == bool(root.val))
+      elif root.val == 2:
+        if needed_result:
+          left_count = self._dp_cached(cache, root.left, needed_result)
+          if left_count == 0:
+            return 0
+          right_count = self._dp_cached(cache, root.right, needed_result)
+          if right_count == 0:
+            return 0
+          return min(left_count, right_count)
+        else:
+          return self._dp_cached(cache, root.left, needed_result) + self._dp_cached(cache, root.right, needed_result)
+      elif root.val == 3:
+        if needed_result:
+          left_count = self._dp_cached(cache, root.left, needed_result)
+          right_count = self._dp_cached(cache, root.right, needed_result)
+          return left_count + right_count
+        else:
+          left_count = self._dp_cached(cache, root.left, needed_result)
+          if left_count == 0:
+            return 0
+          right_count = self._dp_cached(cache, root.right, needed_result)
+          if right_count == 0:
+            return 0
+          return min(left_count, right_count)
+      elif root.val == 4:
+        candidates = []
+        if needed_result:
+          left_count = self._dp_cached(cache, root.left, True) + self._dp_cached(cache, root.right, False)
+          if left_count == 0:
+            return 0
+          right_count = self._dp_cached(cache, root.left, False) + self._dp_cached(cache, root.right, True)
+          if right_count == 0:
+            return 0
+          return min(left_count, right_count)
+        else:
+          left_count = self._dp_cached(cache, root.left, False) + self._dp_cached(cache, root.right, False)
+          if left_count == 0:
+            return 0
+          right_count = self._dp_cached(cache, root.left, True) + self._dp_cached(cache, root.right, True)
+          if right_count == 0:
+            return 0
+          return min(left_count, right_count)
+      else:
+        if root.left:
+          return self._dp_cached(cache, root.left, not needed_result)
+        else:
+          return self._dp_cached(cache, root.right, not needed_result)
