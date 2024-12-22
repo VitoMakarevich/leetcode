f3ab4a2cd91407ca0f9296441ad577584e7e064f
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2024-12-22 16:52:03 +0100

    Time: 99 ms (90.32%), Space: 57.4 MB (81.72%) - LeetHub

--- 2399-minimum-flips-in-binary-tree-to-get-result/2399-minimum-flips-in-binary-tree-to-get-result.py
+++ 2399-minimum-flips-in-binary-tree-to-get-result/2399-minimum-flips-in-binary-tree-to-get-result.py
@@ -1,26 +1,33 @@
+# Definition for a binary tree root.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
     def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
-        def dfs(node):
-            if node.val == 0:
-                return 1, 0
-            if node.val == 1:
-                return 0, 1
+      t, f = self._dp(root)
+      return t if result else f
 
-            if node.val == 5: # NOT
-                t, f = dfs(node.left or node.right)
-                return f, t
+    def _dp(self, root):
+      if root.val == 0:
+          return 1, 0
+      if root.val == 1:
+          return 0, 1
 
-            lt, lf = dfs(node.left)
-            rt, rf = dfs(node.right)
-            
-            if node.val == 2: # OR
-                return min(lt + rt, lf + rt, lt + rf), lf + rf
+      if root.val == 5: # NOT
+          t, f = self._dp(root.left or root.right)
+          return f, t
 
-            if node.val == 3: # AND
-                return lt + rt, min(lt + rf, lf + rt, lf + rf)
-            
-            if node.val == 4: # XOR
-                return min(lf + rt, lt + rf), min(lt + rt, lf + rf)
-        
-        t, f = dfs(root)
-        return  t if result else f
+      lt, lf = self._dp(root.left)
+      rt, rf = self._dp(root.right)
+      
+      if root.val == 2: # OR
+          return min(lt + rt, lf + rt, lt + rf), lf + rf
+
+      if root.val == 3: # AND
+          return lt + rt, min(lt + rf, lf + rt, lf + rf)
+      
+      if root.val == 4: # XOR
+          return min(lf + rt, lt + rf), min(lt + rt, lf + rf)
