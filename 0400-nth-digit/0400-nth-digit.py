1eeb3f3e4cfe309b4dbe59a09eac2f66b9fc9e47
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-05 17:58:16 +0100

    Time: 0 ms (100%), Space: 17.7 MB (25.25%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.py
+++ 0400-nth-digit/0400-nth-digit.py
@@ -1,16 +1,16 @@
 class Solution:
     def findNthDigit(self, n) -> List[int]:
         number_of_digits = 1
-        count_of_digits_with_this_number_of_digits = 9 * pow(10, number_of_digits - 1)
+        count_of_digits_with_this_number_of_digits = 9 * (10 ** (number_of_digits - 1))
         count_of_total_digits = count_of_digits_with_this_number_of_digits * number_of_digits
         while n - count_of_total_digits > 0:
             n -= count_of_total_digits
             number_of_digits += 1
-            count_of_digits_with_this_number_of_digits = 9 * pow(10, number_of_digits - 1)
+            count_of_digits_with_this_number_of_digits = 9 * (10 ** (number_of_digits - 1))
             count_of_total_digits = count_of_digits_with_this_number_of_digits * number_of_digits
         n -= 1
         digit_position = n // number_of_digits
         position_in_number = n % number_of_digits if number_of_digits > 1 else 0
-        result_number = pow(10, number_of_digits - 1) + digit_position
+        result_number = 10 ** (number_of_digits - 1) + digit_position
         
         return int(str(result_number)[position_in_number])
