fd3d5eba4b54fbe53984c227df46ae8792943a13
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-07 20:01:56 +0100

    Time: 363 ms (53.63%), Space: 25.3 MB (26.9%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,23 +1,26 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        
-        # x, y, obstacles, steps
-        q = deque([(0,0,k,0)])
-        seen = set()
-        
-        while q:
-            x, y, left, steps = q.popleft()
-            if (x,y,left) in seen or left<0:
-                continue
-            if (x, y) == (m-1, n-1):
-                return steps
-            seen.add((x,y,left))
-            if grid[x][y] == 1:
-                left-=1
-            for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-                new_x, new_y = x+dx, y+dy
-                if 0<=new_x<m and 0<=new_y<n:
-                    q.append((new_x, new_y, left, steps+1))
-        return -1
-            
+      init = (0, 0, k)
+      rows, cols = len(grid), len(grid[0])
+      q = deque([init])
+      visited = set()
+      visited.add(init)
+      turn = 0
+      while q:
+        for i in range(len(q)):
+          i, j, obstacles = q.popleft()
+          if i == rows - 1 and j == cols - 1:
+            return turn
+          for neigh_i, neigh_j in [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]:
+            if neigh_i >= 0 and neigh_i < rows and neigh_j >= 0 and neigh_j < cols and not (neigh_i, neigh_j, obstacles) in visited:
+              if grid[neigh_i][neigh_j] == 1 and obstacles > 0:
+                visited.add((neigh_i, neigh_j, obstacles))
+                q.append((neigh_i, neigh_j, obstacles - 1))
+              elif grid[neigh_i][neigh_j] == 0:
+                next_step = (neigh_i, neigh_j, obstacles)
+                visited.add(next_step)
+                q.append(next_step)
+        turn += 1
+      return -1
+          
+
