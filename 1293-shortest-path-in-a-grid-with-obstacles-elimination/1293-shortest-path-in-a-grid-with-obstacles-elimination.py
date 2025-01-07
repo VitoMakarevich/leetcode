115e2ea80968ff4e98d300430916d2624af669e0
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-07 19:53:40 +0100

    Time: 853 ms (7.04%), Space: 29.5 MB (5.06%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -10,8 +10,7 @@ class Solution:
         distance, rest = heapq.heappop(q)
         i, j, turn, obstacles = rest
         if i == rows - 1 and j == cols - 1:
-          print(res, turn)
-          res = min(res, turn)
+          return turn
         for neigh_i, neigh_j in [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]:
           if neigh_i >= 0 and neigh_i < rows and neigh_j >= 0 and neigh_j < cols:
             if grid[neigh_i][neigh_j] == 1 and obstacles > 0:
@@ -24,9 +23,9 @@ class Solution:
               if not next_step in visited:
                 visited.add(next_step)
                 q.append(self.create_item(neigh_i, neigh_j, obstacles, turn + 1, rows, cols))
-      return -1 if res == float('inf') else res
+      return -1
     def create_item(self, i, j, obstacles, turn, rows, cols):
-      distance = sqrt((rows - 1 - i) ** 2 + (cols - 1 - j) ** 2) + turn - obstacles
+      distance = rows - 1 - i + cols - 1 - j + turn * turn - obstacles
       return (distance, (i, j, turn, obstacles))
           
 
