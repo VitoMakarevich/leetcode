b20f12f4c02cc6105c494d484493fcea74c289ac
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-07 19:52:19 +0100

    Time: 7100 ms (5%), Space: 129.3 MB (5.06%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -2,30 +2,32 @@ class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
       init = ((0, 0), k)
       rows, cols = len(grid), len(grid[0])
-      q = deque([init])
+      q = [self.create_item(0, 0, k, 0, rows, cols)]
       visited = set()
       visited.add(init)
-      turn = 0
+      res = float('inf')
       while q:
-        for i in range(len(q)):
-          cur_coordinates, obstacles = q.popleft()
-          i, j = cur_coordinates
-          if i == rows - 1 and j == cols - 1:
-            return turn
-          for neigh_i, neigh_j in [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]:
-            if neigh_i >= 0 and neigh_i < rows and neigh_j >= 0 and neigh_j < cols:
-              if grid[neigh_i][neigh_j] == 1 and obstacles > 0:
-                next_step = ((neigh_i, neigh_j), obstacles - 1)
-                if not next_step in visited:
-                  visited.add(next_step)
-                  q.append(next_step)
-              elif grid[neigh_i][neigh_j] == 0:
-                next_step = ((neigh_i, neigh_j), obstacles)
-                if not next_step in visited:
-                  visited.add(next_step)
-                  q.append(next_step)
-        turn += 1
-      return -1
+        distance, rest = heapq.heappop(q)
+        i, j, turn, obstacles = rest
+        if i == rows - 1 and j == cols - 1:
+          print(res, turn)
+          res = min(res, turn)
+        for neigh_i, neigh_j in [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]:
+          if neigh_i >= 0 and neigh_i < rows and neigh_j >= 0 and neigh_j < cols:
+            if grid[neigh_i][neigh_j] == 1 and obstacles > 0:
+              next_step = ((neigh_i, neigh_j), obstacles - 1)
+              if not next_step in visited:
+                visited.add(next_step)
+                heapq.heappush(q, self.create_item(neigh_i, neigh_j, obstacles - 1, turn + 1, rows, cols))
+            elif grid[neigh_i][neigh_j] == 0:
+              next_step = ((neigh_i, neigh_j), obstacles)
+              if not next_step in visited:
+                visited.add(next_step)
+                q.append(self.create_item(neigh_i, neigh_j, obstacles, turn + 1, rows, cols))
+      return -1 if res == float('inf') else res
+    def create_item(self, i, j, obstacles, turn, rows, cols):
+      distance = sqrt((rows - 1 - i) ** 2 + (cols - 1 - j) ** 2) + turn - obstacles
+      return (distance, (i, j, turn, obstacles))
           
 
 
