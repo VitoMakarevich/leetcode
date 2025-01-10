ca499deb048b0c6f837bf06e51a30c069d74886e
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-10 16:23:41 +0100

    Time: 1075 ms (20.73%), Space: 36.8 MB (17.38%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,26 +1,31 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
       self._stones = stones
+      self._cache = {}
       return self.internal(0, 0)
     
-    @cache
+    
     def internal(self, pos, prev_jump):
-      next_jump_options = None
-      if pos == len(self._stones) - 1:
-        return True
-      if prev_jump == 0:
-        next_jump_options = [1]
-      else:
-        if prev_jump == 1:
-          next_jump_options = [1, 2]
+      item = (pos, prev_jump)
+      if not item in self._cache:
+        next_jump_options = None
+        if pos == len(self._stones) - 1:
+          self._cache[item] = True
+        if prev_jump == 0:
+          next_jump_options = [1]
         else:
-          next_jump_options = range(prev_jump - 1, prev_jump + 2)
-      cur = self._stones[pos]
-      for idx, position in enumerate(self._stones[pos + 1:], start = pos + 1):
-        if position > cur + next_jump_options[-1]:
-          break
-        if position - cur in next_jump_options:
-          res = self.internal(idx, position - cur)
-          if res:
-            return True
-      return False
+          if prev_jump == 1:
+            next_jump_options = [1, 2]
+          else:
+            next_jump_options = range(prev_jump - 1, prev_jump + 2)
+        cur = self._stones[pos]
+        for idx, position in enumerate(self._stones[pos + 1:], start = pos + 1):
+          if position > cur + next_jump_options[-1]:
+            break
+          if position - cur in next_jump_options:
+            res = self.internal(idx, position - cur)
+            if res:
+              self._cache[item] = True
+        if not item in self._cache:
+          self._cache[item] = False
+      return self._cache[item]
