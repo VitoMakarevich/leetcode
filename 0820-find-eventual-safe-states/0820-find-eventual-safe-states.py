c22cbaa2b90ae5e9d226ad39d9e1a1254ccd5388
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-01-29 19:42:11 +0100

    Time: 44 ms (53.3%), Space: 24.2 MB (21.64%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -0,0 +1,27 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        visited = set()
+        cache = {}
+        safe_or_terminal = set()
+        for index, adj in enumerate(graph):
+          if not index in visited:
+            is_safe = self._dfs(index, graph, set(), cache)
+            if is_safe:
+              safe_or_terminal.add(index)
+        return sorted(safe_or_terminal)
+    def _dfs(self, node, graph, path, cache):
+      if not node in cache:
+        is_safe = True
+        path.add(node)
+        for adj in graph[node]:
+          if adj in path:
+            res = is_safe = False
+            break
+          else:
+            res = self._dfs(adj, graph, path, cache)
+            if not res:
+              is_safe = False
+              break
+        cache[node] = is_safe
+        path.remove(node)
+      return cache[node]
