2aa30746498ff6a9a4980029151eea34ee255eed
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-02 18:09:59 +0100

    Time: 7 ms (43.33%), Space: 20.8 MB (8.36%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -3,7 +3,6 @@ class Solution:
       cache = defaultdict(list)
       cur_path = []
       target = len(graph) - 1
-      res = []
       def _dfs(node):
         if node == target:
           return [[node]]
@@ -14,7 +13,6 @@ class Solution:
             paths = _dfs(adj)
             if len(paths) > 0:
               for p in paths:
-                res.append(cur_path + p)
                 local_r.append([node] + p)
           cache[node] = local_r
           cur_path.pop()
