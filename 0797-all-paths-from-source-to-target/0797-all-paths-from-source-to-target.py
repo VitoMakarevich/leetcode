32f0f298427071aae291a341e0d52e71337b8184
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-02 18:03:21 +0100

    Time: 11 ms (7.71%), Space: 23.9 MB (8.36%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,24 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+      cache = defaultdict(list)
+      cur_path = []
+      target = len(graph) - 1
+      res = []
+      def _dfs(node):
+        if node == target:
+          return [[node]]
+        if not node in cache:
+          cur_path.append(node)
+          local_r = []
+          for adj in graph[node]:
+            paths = _dfs(adj)
+            if len(paths) > 0:
+              for p in paths:
+                copied_p = cur_path.copy()
+                copied_p.append(p)
+                res.append(copied_p)
+                local_r.append([node] + p)
+          cache[node] = local_r
+          cur_path.pop()
+        return cache[node]
+      return _dfs(0)
