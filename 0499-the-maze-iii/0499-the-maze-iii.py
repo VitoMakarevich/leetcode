940ce8f7a0ae0aee03e1c7d52d47e3acc2c68bc2
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-21 18:08:23 +0100

    Time: 7 ms (74.44%), Space: 17.8 MB (93.82%) - LeetHub

--- 0499-the-maze-iii/0499-the-maze-iii.py
+++ 0499-the-maze-iii/0499-the-maze-iii.py
@@ -22,48 +22,19 @@ class Solution:
           
     def _get_new_positions(self, current, maze, width, height, target):
       options = []
-      #right
-      current_x, current_y = current
-      while current_x + 1 < width:
-        if target[0] == current_x and target[1] == current_y:
-          break
-        if maze[current_y][current_x + 1] == 0:
-          current_x += 1
-        else:
-          break
-      options.append(((current_x, current_y), current_x - current[0], "r"))
-      #left
-      current_x, current_y = current
-      while current_x - 1 >= 0:
-        if target[0] == current_x and target[1] == current_y:
-          break
-        if maze[current_y][current_x - 1] == 0:
-          current_x -= 1
-        else:
-          break
-      options.append(((current_x, current_y), current[0] - current_x, "l"))
-      #bottom
-      current_x, current_y = current
-      while current_y + 1 < height:
-        if target[0] == current_x and target[1] == current_y:
-          break
-        if maze[current_y + 1][current_x] == 0:
-          current_y += 1
-        else:
-          break
-      options.append(((current_x, current_y), current_y - current[1], "d"))
-      #up
-      current_x, current_y = current
-      while current_y - 1 >= 0:
-        if target[0] == current_x and target[1] == current_y:
-          break
-        if maze[current_y - 1][current_x] == 0:
-          current_y -= 1
-        else:
-          break
-      options.append(((current_x, current_y), current[1] - current_y, "u"))
+      for incr, direction in [((0, -1), 'u'), ((0, 1), 'd'), ((-1, 0), 'l'), ((1, 0), 'r')]:
+        current_x, current_y = current
+        nc = current
+        while nc[0] >= 0 and nc[0] < width and nc[1] >= 0 and nc[1] < height and maze[nc[1]][nc[0]] == 0:
+          if target == nc:
+            break
+          new = (nc[0] + incr[0], nc[1] + incr[1])
+          if new[0] >= 0 and new[0] < width and new[1] >= 0 and new[1] < height and maze[new[1]][new[0]] == 0:
+            nc = new
+          else:
+            break
+        options.append((nc, abs(nc[0] - current[0]) + abs(nc[1] - current[1]), direction))
       return options
-
 
     def _calculate_item(self, current, target, distance, path):
       diff = sqrt(abs(target[0] - current[0]) ** 2 + abs(target[1] - current[1]) ** 2)
