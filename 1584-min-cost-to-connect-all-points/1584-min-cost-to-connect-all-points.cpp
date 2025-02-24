6a45c398e3719da0d8462b8ccdb5cf2818728270
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-24 17:03:55 +0100

    Time: 454 ms (41.44%), Space: 47.3 MB (71.2%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -40,29 +40,32 @@ private:
 
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
+  int minCostConnectPoints(std::vector<std::vector<int>>& points) {
       int sum = 0;
+      int n = points.size();
+      std::vector<bool> visited(n, false);
       using VertexDistance = std::tuple<int, int>;
-      std::unordered_set<int> not_visited(points.size());
-      for (int i = 0; i < points.size(); ++i) {
-        not_visited.insert(i);
-      }
       std::priority_queue<VertexDistance, std::vector<VertexDistance>, std::greater<VertexDistance>> pq;
       pq.push(std::make_tuple(0, 0));
 
-      while(!pq.empty()) {
-        VertexDistance vd = pq.top();
-        pq.pop();
-        auto [distance, vertex] = vd;
-        if (not_visited.find(vertex) != not_visited.end()) {
+      while (!pq.empty()) {
+          auto [distance, vertex] = pq.top();
+          pq.pop();
+
+          if (visited[vertex]) continue;
+          visited[vertex] = true;
+
           sum += distance;
-          not_visited.erase(vertex);
-          for (auto pointIdx: not_visited) {
-              auto candidateDistance = abs(points[pointIdx][0] - points[vertex][0]) + abs(points[pointIdx][1] - points[vertex][1]);
-              pq.push(std::make_tuple(candidateDistance, pointIdx));
+
+          for (int pointIdx = 0; pointIdx < n; ++pointIdx) {
+              if (!visited[pointIdx]) {
+                  int candidateDistance = std::abs(points[pointIdx][0] - points[vertex][0]) +
+                                          std::abs(points[pointIdx][1] - points[vertex][1]);
+                  pq.push(std::make_tuple(candidateDistance, pointIdx));
+              }
           }
-        }
       }
+
       return sum;
-    }
+  }
 };
