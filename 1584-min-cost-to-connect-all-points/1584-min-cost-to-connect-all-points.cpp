8d91061b3e5beeff49516b0b1c378eb08e5f071a
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-24 16:56:18 +0100

    Time: 430 ms (47.23%), Space: 47.9 MB (71.06%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,69 @@
+class UnionFind {
+public:
+  UnionFind(int size): parent(size), sizes(size, 1) {
+    for(int i = 0; i < size; i++) {
+      parent[i] = i;
+    }
+  }
+
+  int find(int node) {
+    int r = parent[node];
+    if (r != node)
+      parent[node] = find(parent[node]);
+    return r;
+  }
+
+  void unionNodes(int one, int two) {
+    int oneRoot = find(one);
+    int twoRoot = find(two);
+
+    if (oneRoot != twoRoot) {
+      if(sizes[twoRoot] > sizes[oneRoot]) {
+        parent[oneRoot] = twoRoot;
+      } else if (sizes[oneRoot] > sizes[twoRoot]) {
+        parent[twoRoot] = oneRoot;
+      } else {
+        parent[twoRoot] = oneRoot;
+        sizes[oneRoot] += 1;
+      }
+    }
+  }
+
+  bool isConnected(int one, int two) {
+    return find(one) == find(two);
+  }
+
+private:
+  std::vector<int> parent;
+  std::vector<int> sizes;
+};
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+      int sum = 0;
+      using VertexDistance = std::tuple<int, int>;
+      std::vector<int> visited(points.size(), false);
+      std::priority_queue<VertexDistance, std::vector<VertexDistance>, std::greater<VertexDistance>> pq;
+      pq.push(std::make_tuple(0, 0));
+
+      while(!pq.empty()) {
+        VertexDistance vd = pq.top();
+        pq.pop();
+        auto [distance, vertex] = vd;
+        if (!visited[vertex]) {
+          sum += distance;
+          visited[vertex] = true;
+          size_t pointIdx = 0;
+          for (auto value: visited) {
+            if (!value) {
+              auto candidateDistance = abs(points[pointIdx][0] - points[vertex][0]) + abs(points[pointIdx][1] - points[vertex][1]);
+              pq.push(std::make_tuple(candidateDistance, pointIdx));
+            }
+            ++pointIdx;
+          }
+        }
+      }
+      return sum;
+    }
+};
