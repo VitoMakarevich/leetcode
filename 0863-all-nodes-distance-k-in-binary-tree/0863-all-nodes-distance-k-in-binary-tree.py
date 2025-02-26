5f2765cbca75c985146b7c9a97daf95260a5e4e5
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-26 14:26:49 +0100

    Time: 36 ms (89.31%), Space: 18 MB (70.01%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        graph = defaultdict(list)
+        def build_g(node):
+          if node.left:
+            graph[node.val].append(node.left.val)
+            graph[node.left.val].append(node.val)
+            build_g(node.left)
+          if node.right:
+            graph[node.val].append(node.right.val)
+            graph[node.right.val].append(node.val)
+            build_g(node.right)
+        build_g(root)
+        visited = set()
+        cur_level = 0
+        q = deque([target.val])
+        while len(q) > 0 and cur_level < k:
+          for i in range(len(q)):
+            cur = q.popleft()
+            visited.add(cur)
+            for neighbor_v in graph[cur]:
+              if not neighbor_v in visited:
+                q.append(neighbor_v)
+          cur_level += 1
+        return list(q)
+        
+    
+    # def add_level(self, node, level, res):
+    #   cur_level = 0
+    #   q = deque(node)
+    #   while cur_level < level:
+    #     for i in range(len(q)):
+    #       cur = q.popleft()
+    #       if cur.left:
+    #         q.append(cur.left)
+    #       if cur.right:
+    #         q.append(cur.right)
+    #     cur_level += 1
+    #   for v in q:
+    #     res.append(v)
