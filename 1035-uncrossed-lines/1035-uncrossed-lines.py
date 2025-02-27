f4d287138ebd8b740db8453f3bd440ea3d8f1d20
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-02-27 16:47:05 +0100

    Time: 1546 ms (5.07%), Space: 18 MB (88.1%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -5,15 +5,18 @@ class Solution:
        self._nums1 = nums1
        self._nums2 = nums2
 
-       return self._solve(0, 0)
-    @cache
-    def _solve(self, left_pos, right_pos):
-      cand = []
-      for l in range(left_pos, self._m):
-        for r in range(right_pos, self._n):
-          if self._nums1[l] == self._nums2[r]:
-            cand.append(self._solve(l + 1, r + 1))
-      if len(cand) > 0:
-        return 1 + max(cand)
-      
-      return 0
+       return self._solve(0, 0, {})
+    
+    def _solve(self, left_pos, right_pos, cache):
+      cache_key = f"{left_pos}-{right_pos}"
+      if not cache_key in cache:
+        cand = []
+        for l in range(left_pos, self._m):
+          for r in range(right_pos, self._n):
+            if self._nums1[l] == self._nums2[r]:
+              cand.append(self._solve(l + 1, r + 1, cache))
+        if len(cand) > 0:
+          cache[cache_key] = 1 + max(cand)
+        else:
+          cache[cache_key] = 0
+      return cache[cache_key]
