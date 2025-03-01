03d8d672aa6ca63c5628f11cc5967c0f32891898
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-03-01 14:07:15 +0100

    Time: 426 ms (81.79%), Space: 42.8 MB (72.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,17 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-      self.text1 = text1
-      self.text2 = text2
-      cache = {}
-      return self.solve(0, 0, cache)
-
-    def solve(self, p1, p2, cache):
-      key = (p1, p2)
-      if not key in cache:
-        if p1 == len(self.text1) or p2 == len(self.text2):
-          cache[key] = 0
-        elif self.text1[p1] == self.text2[p2]:
-          cache[key] = 1 + self.solve(p1 + 1, p2 + 1, cache)
-        else:
-          cache[key] = max(self.solve(p1 + 1, p2, cache), self.solve(p1, p2 + 1, cache))
-      return cache[key]
+        
+        # Make a grid of 0's with len(text2) + 1 columns 
+        # and len(text1) + 1 rows.
+        dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        
+        # Iterate up each column, starting from the last one.
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                # If the corresponding characters for this cell are the same...
+                if text2[col] == text1[row]:
+                    dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
+                # Otherwise they must be different...
+                else:
+                    dp_grid[row][col] = max(dp_grid[row + 1][col], dp_grid[row][col + 1])
+        
+        # The original problem's answer is in dp_grid[0][0]. Return it.
+        return dp_grid[0][0]
