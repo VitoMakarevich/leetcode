675c3f786979e5fcea8dd50694a19aec2d1c9884
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-03-01 14:05:05 +0100

    Time: 1377 ms (13.94%), Space: 185.3 MB (19.1%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,12 +2,16 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
       self.text1 = text1
       self.text2 = text2
-      return self.solve(0, 0)
-    @cache
-    def solve(self, p1, p2):
-      if p1 == len(self.text1) or p2 == len(self.text2):
-        return 0
-      if self.text1[p1] == self.text2[p2]:
-        return 1 + self.solve(p1 + 1, p2 + 1)
-      else:
-        return max(self.solve(p1 + 1, p2), self.solve(p1, p2 + 1))
+      cache = {}
+      return self.solve(0, 0, cache)
+
+    def solve(self, p1, p2, cache):
+      key = (p1, p2)
+      if not key in cache:
+        if p1 == len(self.text1) or p2 == len(self.text2):
+          cache[key] = 0
+        elif self.text1[p1] == self.text2[p2]:
+          cache[key] = 1 + self.solve(p1 + 1, p2 + 1, cache)
+        else:
+          cache[key] = max(self.solve(p1 + 1, p2, cache), self.solve(p1, p2 + 1, cache))
+      return cache[key]
