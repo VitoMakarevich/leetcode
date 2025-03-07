08e75378a6d6851427360e711b6fe143c34d2ded
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-03-07 15:17:09 +0100

    Time: 1386 ms (5.04%), Space: 37.6 MB (20.75%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,15 +1,17 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-      return self._internal(s, {})
+      return self._internal(s, 0, len(s) - 1,  {})
 
-    def _internal(self, str, cache):
-      if len(str) == 1 or len(str) == 0:
+    def _internal(self, str, left, right, cache):
+      if left >= right:
         return 0
-      if str in cache:
-        return cache[str]
+      v = (left, right)
+      if v in cache:
+        return cache[v]
       else:
-        if str[0] == str[-1]:
-          cache[str] = self._internal(str[1:-1], cache)
+        print(left, right)
+        if str[right] == str[left]:
+          cache[v] = self._internal(str, left + 1, right - 1, cache)
         else:
-          cache[str] = 1 + min(self._internal(str[1:], cache), self._internal(str[:-1], cache))
-      return cache[str]
+          cache[v] = 1 + min(self._internal(str, left + 1, right, cache), self._internal(str, left, right - 1, cache))
+      return cache[v]
