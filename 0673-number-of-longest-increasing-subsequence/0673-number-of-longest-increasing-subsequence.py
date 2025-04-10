c65a10242efb67caf859baa326091139da4f312f
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-10 14:22:57 +0200

    Time: 489 ms (58.78%), Space: 17.9 MB (89.53%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,25 +1,21 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-      self._nums = nums
-      memo = {}
-      return self._dp(0, float('-inf'), memo)[1]
-        
-    def _dp(self, i: int, prev: int, memo) -> int:
-        if i == len(self._nums):
-          return 0, 1
-        key = (i, prev)
-        if not key in memo:
-          taken_length = 0
-          taken_count = 0
-          with_this = 0
-          if self._nums[i] > prev:
-            taken_length, taken_count = self._dp(i + 1, self._nums[i], memo)
-            taken_length += 1
-          not_taken_length, not_taken_count = self._dp(i + 1, prev, memo)
-          if taken_length > not_taken_length:
-            memo[key] = taken_length, taken_count
-          elif taken_length == not_taken_length:
-            memo[key] = taken_length, taken_count + not_taken_count
-          else:
-            memo[key] = not_taken_length, not_taken_count
-        return memo[key]
+      if not nums:
+        return 0
+      lengths = [1] * len(nums)
+      counts = [1] * len(nums)
+
+      for left in range(len(nums)):
+        for right in range(left + 1, len(nums)):
+          if nums[right] > nums[left]:
+            taken_length = lengths[left] + 1
+            not_taken_length = lengths[right]
+            if taken_length > not_taken_length:
+              lengths[right] = taken_length
+              counts[right] = counts[left] 
+            elif taken_length == not_taken_length:
+              lengths[right] = taken_length
+              counts[right] += counts[left]
+      
+      max_length = max(lengths)
+      return sum(count for length, count in zip(lengths, counts) if length == max_length)
