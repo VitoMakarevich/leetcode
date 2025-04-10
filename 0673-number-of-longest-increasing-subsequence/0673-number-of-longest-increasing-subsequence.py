90d79a0088255a1c3f7c444a345b49779cb2df3e
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-10 14:22:53 +0200

    Time: 2499 ms (5.12%), Space: 470.6 MB (5.24%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,23 +1,25 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
       self._nums = nums
-      return self._dp(0, float('-inf'))[1]
+      memo = {}
+      return self._dp(0, float('-inf'), memo)[1]
         
-    @cache
-    def _dp(self, i: int, prev: int) -> int:
+    def _dp(self, i: int, prev: int, memo) -> int:
         if i == len(self._nums):
           return 0, 1
-        
-        taken_length = 0
-        taken_count = 0
-        with_this = 0
-        if self._nums[i] > prev:
-          taken_length, taken_count = self._dp(i + 1, self._nums[i])
-          taken_length += 1
-        not_taken_length, not_taken_count = self._dp(i + 1, prev)
-        if taken_length > not_taken_length:
-          return taken_length, taken_count
-        elif taken_length == not_taken_length:
-          return taken_length, taken_count + not_taken_count
-        else:
-          return not_taken_length, not_taken_count
+        key = (i, prev)
+        if not key in memo:
+          taken_length = 0
+          taken_count = 0
+          with_this = 0
+          if self._nums[i] > prev:
+            taken_length, taken_count = self._dp(i + 1, self._nums[i], memo)
+            taken_length += 1
+          not_taken_length, not_taken_count = self._dp(i + 1, prev, memo)
+          if taken_length > not_taken_length:
+            memo[key] = taken_length, taken_count
+          elif taken_length == not_taken_length:
+            memo[key] = taken_length, taken_count + not_taken_count
+          else:
+            memo[key] = not_taken_length, not_taken_count
+        return memo[key]
