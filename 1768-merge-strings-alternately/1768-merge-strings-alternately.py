8e013793edd8454fd9662e928d2c9496ef798f07
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-11 14:34:06 +0200

    Time: 35 ms (76.37%), Space: 17.6 MB (94.7%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,12 +3,11 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
        buffer = StringIO()
        l1, l2 = len(word1), len(word2)
-       i, j = 0, 0
-       while i < l1 and j < l2:
+       i = 0
+       while i < l1 and i < l2:
          buffer.write(word1[i])
-         buffer.write(word2[j])
+         buffer.write(word2[i])
          i += 1
-         j += 1
        buffer.write(word1[i:])
-       buffer.write(word2[j:])
+       buffer.write(word2[i:])
        return buffer.getvalue()  
