0f2ec020c28d8ce3fcb0e0e666d733d5fccfc3ba
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-14 10:21:41 +0200

    Time: 41 ms (5.02%), Space: 19.8 MB (75.25%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,16 +1,29 @@
-from functools import cmp_to_key
+from sortedcontainers import SortedDict
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x: x[0])
-        pq = []
-        res = 0
-        for interval in intervals + [(100000000, 100000000)]:
-          if len(pq) == 0 or pq[0] > interval[0]:
-            heapq.heappush(pq, interval[1])
-          else:
-            res = max(len(pq), res)
-            while pq and pq[0] <= interval[0]:
-              heapq.heappop(pq)
-            heapq.heappush(pq, interval[1])
-        return res
+      max_used = 0
+      pq = []
+      intervals.sort()
+      for interval in intervals + [(100000000, 100000000)]:
+        if not pq or pq[0] > interval[0]:
+          heapq.heappush(pq, interval[1])
+        else:
+          while pq and pq[0] <= interval[0]:
+            heapq.heappop(pq)
+          heapq.heappush(pq, interval[1])
+        max_used = max(max_used, len(pq)) 
+       
+      return max_used
+        # intervals.sort()
+        # events = SortedDict(int)
+        # change_times = set()
+        # for start, end in intervals:
+        #     events[start] = events.get(start, 0) + 1
+        #     events[end] = events.get(end, 0) - 1
+        # current_rooms = 0
+        # max_rooms_in_use = 0
+        # for time, change in events.items():
+        #     current_rooms += change
+        #     max_rooms_in_use = max(max_rooms_in_use, current_rooms)
+        # return max_rooms_in_use
