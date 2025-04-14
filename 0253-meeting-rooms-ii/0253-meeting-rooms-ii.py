f5358f49dc4a33ee77e93ced8977968313de017d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-14 10:22:36 +0200

    Time: 51 ms (5.02%), Space: 19.8 MB (75.25%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,18 +1,16 @@
-from sortedcontainers import SortedDict
+from functools import cmp_to_key
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-      max_used = 0
-      pq = []
-      intervals.sort(key = lambda x: x[0])
-      for interval in intervals + [(100000000, 100000000)]:
-        if not pq or pq[0] > interval[0]:
-          heapq.heappush(pq, interval[1])
-        else:
-          while pq and pq[0] <= interval[0]:
-            heapq.heappop(pq)
-          heapq.heappush(pq, interval[1])
-        max_used = max(max_used, len(pq)) 
-       
-      return max_used
-
+        intervals.sort(key=lambda x: x[0])
+        pq = []
+        res = 0
+        for interval in intervals + [(100000000, 100000000)]:
+          if len(pq) == 0 or pq[0] > interval[0]:
+            heapq.heappush(pq, interval[1])
+          else:
+            res = max(len(pq), res)
+            while pq and pq[0] <= interval[0]:
+              heapq.heappop(pq)
+            heapq.heappush(pq, interval[1])
+        return res
