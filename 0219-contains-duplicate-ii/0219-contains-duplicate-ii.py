a60961d86647b1cc96e0be78a2e82c4da0644e12
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-15 21:14:23 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-      storage = defaultdict(list)
+      sliding_window = {}
+      # for each step - check for previous occurence of this number
+      #   yes - check length diff and return early if <= k
+      # add index into window
+      # if index >= k:
+      # remove last seen index of value at index - k
       for index, value in enumerate(nums):
-        storage[value].append(index)
-      for indexes in storage.values():
-        if len(indexes) > 1:
-          prev = indexes[0]
-          for next_index in indexes[1:]:
-            if next_index - prev <= k:
-              return True
-            prev = next_index
+        if value in sliding_window and index - sliding_window[value] <= k:
+          return True
+        if index >= k:
+          del sliding_window[nums[index - k]]
+        sliding_window[value] = index
       return False
+          
