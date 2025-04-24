84e95c54306a485315c28edf782f43a8796a2663
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-24 20:41:13 +0200

    Time: 0 ms (100%), Space: 18.1 MB (67.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,37 +2,25 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-        biggest_pos = -1
-        while True:
-            mid = floor((right - left) / 2) + left
-            if nums[left] <= nums[mid] and nums[mid] <= nums[right]:
-                biggest_pos = right
-                break
-            elif nums[left] > nums[right] and not nums[mid] > nums[left]:
-                right = mid
-            else:
-                left = mid
-
-        if biggest_pos == len(nums) - 1:
-            return self._find(0, len(nums) - 1, target, nums)
+        while left < right:
+          mid = (left + right) // 2
+          if nums[mid] > nums[-1]:
+            left = mid + 1
+          else:
+            right = mid
+        lowest_element = left
+        biggest_element = lowest_element - 1 if lowest_element > 0 else len(nums) - 1
+        found_in_smaller_array = self._search(nums, lowest_element, len(nums) - 1, target)
+        if found_in_smaller_array == -1:
+          return self._search(nums, 0, biggest_element, target)
+        return found_in_smaller_array
+    def _search(self, nums, left, right, target):
+      while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+          return mid
+        elif nums[mid] > target:
+          right = mid - 1
         else:
-            left_side_range = range(nums[0], nums[biggest_pos] + 1)
-            if target in left_side_range:
-                return self._find(0, biggest_pos, target, nums)
-            else:
-                right_side_range = range(nums[biggest_pos + 1], nums[len(nums) - 1] + 1)
-                if target in right_side_range:
-                    return self._find(biggest_pos + 1, len(nums) - 1, target, nums)
-            return -1
-    
-    def _find(self, left, right, target, nums):
-        while left <= right:
-            mid = floor((right - left) / 2) + left
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return -1
-        
+          left = mid + 1
+      return -1
