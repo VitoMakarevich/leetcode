acad8b35a78b045da35064f48da4b8fcc225fb35
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-04-25 20:39:55 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low = 1
+        high = num // 2
+        while low <= high:
+          mid = (low + high) // 2
+          candidate = mid * mid
+          if candidate == num:
+            return True
+          elif candidate > num:
+            high = mid - 1
+          else:
+            low = high + 1
+        return False if num != 1 else True
