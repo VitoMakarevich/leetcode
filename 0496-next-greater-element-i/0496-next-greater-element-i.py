08f4cd94504cf88ffde40e81cbdf75f61c233747
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-10 15:09:22 +0200

    Time: 3 ms (42.04%), Space: 18 MB (65.5%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pos_in_right = {}
-        for index, value in enumerate(nums2):
-          pos_in_right[value] = index
-        res = []
-        for v in nums1:
-          out = -1
-          for index in range(pos_in_right[v] + 1, len(nums2)):
-            if nums2[index] > v:
-              out = nums2[index]
-              break
-          res.append(out)
+        queue = deque()
+        idx_to_max = {}
+        for index in range(len(nums2)):
+          while queue and nums2[index] > nums2[queue[-1]]:
+            prev = queue.pop()
+            idx_to_max[nums2[prev]] = nums2[index]
+          queue.append(index)
+        for index in queue:
+          idx_to_max[nums2[index]] = -1
+        
+        res = [idx_to_max[v] for v in nums1]
         return res
+        
