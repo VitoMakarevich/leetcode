2e1a51c6d6d3fba6a6a9572bd4dad8fa0437a21c
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-11 13:44:38 +0200

    Time: 74 ms (97.41%), Space: 38.2 MB (46.91%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -1,24 +1,26 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        rows, cols = len(grid), len(grid[0])
-        prefix_sum = [[0] * (len(grid[0]) + 1) for _ in range(len(grid) + 1)]
-        total_sum = 0
-        for i in range(1, len(grid) + 1):
-          for j in range(1, len(grid[0]) + 1):
-            total_sum += grid[i - 1][j - 1]
-            prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] + grid[i - 1][j - 1] - prefix_sum[i - 1][j - 1]
-        if not total_sum % 2 == 0:
-          return False
-
-        target = int(total_sum / 2)
-        col_sum = prefix_sum[rows][1:]
-        res = bisect_left(col_sum, target)
-        if prefix_sum[rows][res + 1] == target:
-          return True
-
-        sum_vector_for_cols = []
-        for i in range(len(grid)):
-          sum_vector_for_cols.append(prefix_sum[i + 1][cols])
         
-        res = bisect_left(sum_vector_for_cols, target)
-        return prefix_sum[res + 1][cols] == target
+        total_sum = sum([sum(r) for r in grid])
+        if total_sum % 2 == 1:
+          return False
+        target = total_sum / 2
+        def _s(matrix):
+          s = 0
+          for row in matrix:
+            s += sum(row)
+            if s == target:
+              return True
+            if s > target:
+              break
+          return False
+        return _s(grid) or _s(zip(*grid))
+        # rows, cols = len(grid), len(grid[0])
+        # prefix_sum = [[0] * (len(grid[0]) + 1) for _ in range(len(grid) + 1)]
+        # total_sum = 0
+        # for i in range(1, len(grid) + 1):
+        #   for j in range(1, len(grid[0]) + 1):
+        #     prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] + grid[i - 1][j - 1] - prefix_sum[i - 1][j - 1]
+        #     if prefix_sum[i][j] == target and (i == rows or j == cols):
+        #       return True
+        # return False
