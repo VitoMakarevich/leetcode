fa57ff9155ef036156c16b8a22bd5c723c8ea590
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-11 03:13:18 +0200

    Time: 151 ms (82.41%), Space: 30.9 MB (86.13%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,23 +1,18 @@
-class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        hq = []
-        res = []
-        to_delete = {}
-        for i, v in enumerate(nums):
-            heapq.heappush(hq, -v)
-            if len(hq) >= k:
-                idx_to_be_removed = i - k
-                if idx_to_be_removed >= 0:
-                # to be removed from PQ - value as in PQ
-                    to_be_removed = -nums[i - k]
-                    # increase remove counter by 1
-                    to_delete[to_be_removed] = to_delete.get(to_be_removed, 0) + 1
-                    while to_delete.get(hq[0], 0) > 0:
-                        top = hq[0]
-                        heapq.heappop(hq)
-                        to_delete[top] -= 1
-
-                res.append(-hq[0])
-        
-        return res
-                
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = collections.deque()
+        out = []
+        for i, n in enumerate(nums):
+            while d and nums[d[-1]] < n:
+                d.pop()
+            d.append(i)
+            if d[0] == i - k:
+                d.popleft()
+            if i>=k-1:
+                out.append(nums[d[0]])
+        return out
