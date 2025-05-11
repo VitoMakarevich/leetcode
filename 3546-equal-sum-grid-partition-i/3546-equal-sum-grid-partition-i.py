09ed3161970cbe7594073ac741f003642f5ad08e
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-11 12:26:32 +0200

    Time: 360 ms (5.04%), Space: 46 MB (6.58%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -7,32 +7,18 @@ class Solution:
           for j in range(1, len(grid[0]) + 1):
             total_sum += grid[i - 1][j - 1]
             prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] + grid[i - 1][j - 1] - prefix_sum[i - 1][j - 1]
-        
-        low, high = 0, rows - 1
+        if not total_sum % 2 == 0:
+          return False
 
-        while low <= high:
-          mid = (low + high) // 2
-          one_part_sum = prefix_sum[mid][cols]
-          second_part_sum = total_sum - one_part_sum
-          if one_part_sum == second_part_sum:
-            return True
-          elif one_part_sum < second_part_sum:
-            low = mid + 1
-          else:
-            high = mid - 1
+        target = int(total_sum / 2)
+        col_sum = prefix_sum[rows][1:]
+        res = bisect_left(col_sum, target)
+        if prefix_sum[rows][res + 1] == target:
+          return True
 
-        low, high = 0, cols - 1
-
-        while low <= high:
-          mid = (low + high) // 2
-          one_part_sum = prefix_sum[rows][mid]
-          second_part_sum = total_sum - one_part_sum
-          if one_part_sum == second_part_sum:
-            return True
-          elif one_part_sum < second_part_sum:
-            low = mid + 1
-          else:
-            high = mid - 1
+        sum_vector_for_cols = []
+        for i in range(len(grid)):
+          sum_vector_for_cols.append(prefix_sum[i + 1][cols])
         
-        
-        return False
+        res = bisect_left(sum_vector_for_cols, target)
+        return prefix_sum[res + 1][cols] == target
