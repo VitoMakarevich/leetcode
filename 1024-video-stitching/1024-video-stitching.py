507858439cb23d34341343ff127dfacde5f4623d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-14 15:14:30 +0200

    Time: 0 ms (100%), Space: 17.8 MB (63.32%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.py
@@ -0,0 +1,23 @@
+class Solution:
+    def videoStitching(self, clips: List[List[int]], time: int) -> int:
+        if time == 0: 
+          return 0
+        clips = sorted([(x, y) for idx, (x, y) in enumerate(clips)], key = lambda x: (x[0], -x[1]))
+        if clips[0][0] > 0 or (len(clips) == 1 and clips[0][1] < time):
+          return -1
+        ans = 1
+        picked_y = clips[0][1]
+        idx = 1
+        while picked_y < time:
+          next_y = picked_y
+          while idx < len(clips) and  clips[idx][0] <= picked_y:
+            if clips[idx][1] > next_y:
+              next_y = clips[idx][1]
+            idx += 1
+          if next_y == picked_y:
+            return -1
+          ans += 1
+          picked_y = next_y
+          
+
+        return ans
