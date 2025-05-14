6e07d8187ae7effe5aabe656271bc497947d872b
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-14 19:39:08 +0200

    Time: 21 ms (84.63%), Space: 18.5 MB (53.9%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,27 +1,18 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-      intervals = []
-      for index in range(len(ranges)):
-        cur_range = ranges[index]
-        if cur_range != 0:
-          intervals.append(
-            (
-              max(0, index - cur_range), min(index + cur_range, n)
-            )
-          )
-      intervals.sort(key = lambda x: (x[0], -x[1]))
-      if len(intervals) == 0:
-        return -1
-      prev_interval_end = intervals[0][1]
-      ans = 1
-      idx = 1
-      while prev_interval_end < n:
-        next_interval_end = prev_interval_end
-        while idx < len(intervals) and intervals[idx][0] <= prev_interval_end:
-          next_interval_end = max(next_interval_end, intervals[idx][1])
-          idx += 1
-        if next_interval_end == prev_interval_end:
+      max_reach = [0] * (n + 1)
+      for i in range(len(ranges)):
+        start = max(0, i - ranges[i])
+        end = min(n, i + ranges[i])
+        max_reach[start] = max(max_reach[start], end)
+      taps = 0
+      curr_end = 0
+      next_end = 0
+      for i in range(n + 1):
+        if i > next_end:
           return -1
-        ans += 1
-        prev_interval_end = next_interval_end
-      return ans
+        if i > curr_end:
+          taps += 1
+          curr_end = next_end
+        next_end = max(next_end, max_reach[i])
+      return taps
