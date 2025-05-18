52fa5b28f67424f8e927533566d296ff87fe9da1
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-18 22:17:47 +0200

    Time: 59 ms (20.78%), Space: 31.4 MB (18.39%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,8 +1,8 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        s = []
+        s = deque()
         for idx in range(len(heights)):
           while s and heights[idx] >= heights[s[-1]]:
             s.pop()
           s.append(idx)
-        return s
+        return list(s)
