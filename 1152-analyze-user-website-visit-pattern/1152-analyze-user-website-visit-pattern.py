2ac2c1763d0f3bb1b44ce6fc75157aead9c1631f
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-20 12:18:26 +0200

    Time: 7 ms (80.5%), Space: 18.9 MB (21.8%) - LeetHub

---
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        visits_by_user = defaultdict(list)
+        visits_by_website = defaultdict(set)
+        for user, ts, website in zip(username, timestamp, website):
+          visits_by_user[user].append((ts, website))
+        pattern_and_score = defaultdict(set)
+        for user in visits_by_user.keys():
+          visits_by_user[user].sort(key = lambda x: x[0])
+          sites = list(map(lambda x: x[1], visits_by_user[user]))
+          for idx1, s1 in enumerate(sites):
+            for idx2, s2 in enumerate(sites[idx1 + 1:], start = idx1 + 1):
+              for s3 in sites[idx2 + 1:]:
+                pattern = (s1, s2, s3)
+
+                pattern_and_score[pattern].add(user)
+        res = ()
+        count = 0
+        for pattern, users in pattern_and_score.items():
+          score = len(users)
+          if score > count or (score == count and pattern < res) or not res:
+            count = score
+            res = pattern
+        return res
+
+        
