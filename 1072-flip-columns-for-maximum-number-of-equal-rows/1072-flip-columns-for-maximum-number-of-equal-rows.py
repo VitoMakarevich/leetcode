1bb4666d5739c2585a2af7e6f3efb99bd7410525
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-20 15:36:26 +0200

    Time: 103 ms (22.51%), Space: 20.3 MB (80.86%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -3,9 +3,7 @@ class Solution:
         res = 0
         counter = defaultdict(int)
         for row in matrix:
-          row_str = ''.join(str(x) for x in row)
-          opp_row = ''.join(map(lambda x: str(1 - x), row))
+          row_str = ''.join(str(x ^ row[0]) for x in row)
           counter[row_str] += 1
-          counter[opp_row] += 1
-          res = max(res, counter[row_str], counter[opp_row])
-        return res
+          
+        return max(counter.values())
