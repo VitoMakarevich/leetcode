04dfd6f872fb810b11c49554a345548151599871
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-22 16:27:51 +0200

    Time: 7 ms (40.22%), Space: 17.8 MB (44.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_store = [set() for _ in range(9)]
-        column_store = [set() for _ in range(9)]
-        segment_storage = [set() for _ in range(9)]
+        row_store = [0 for _ in range(9)]
+        column_store = [0 for _ in range(9)]
+        segment_storage = [0 for _ in range(9)]
+        def i_ex(num, i):
+          return num & 1 << i
+        def i_s(num, i):
+          return num | 1 << i
 
         for row in range(len(board)):
           for column in range(len(board[0])):
             candidate = board[row][column]
             if candidate.isdigit():
+              candidate = int(candidate)
               segment = 3 * (row // 3) + column // 3
-              if candidate in row_store[row] or candidate in column_store[column] or candidate in segment_storage[segment]:
+              if i_ex(row_store[row], candidate) or i_ex(column_store[column], candidate) or i_ex(segment_storage[segment], candidate):
                 return False
-              row_store[row].add(candidate)
-              column_store[column].add(candidate)
-              segment_storage[segment].add(candidate)
+              row_store[row] = i_s(row_store[row], candidate)
+              column_store[column] = i_s(column_store[column], candidate)
+              segment_storage[segment] = i_s(segment_storage[segment], candidate)
         return True
-
+    
         
