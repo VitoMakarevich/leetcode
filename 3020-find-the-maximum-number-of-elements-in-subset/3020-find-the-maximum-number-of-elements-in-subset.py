6891912cfed845e5c394ab80b6ce45875e4536a2
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-27 19:38:32 +0200

    Time: 186 ms (27.35%), Space: 32.5 MB (11.72%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
@@ -8,7 +8,12 @@ class Solution:
         if num in visited:
           continue
         if num == 1:
-          res = max(res, 1)
+          if counter[num] > 1 and counter[num] % 2 == 0:
+            res = counter[num] - 1
+          elif counter[num] > 1 and counter[num] % 2 == 1:
+            res = counter[num]
+          else:
+            res = max(res, counter[num] )
           continue
         visited.add(num)
         prev_num = num
