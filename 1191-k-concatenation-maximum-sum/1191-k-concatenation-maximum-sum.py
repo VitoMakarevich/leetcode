a5e4e3cf3dcdcf1b62d325f3bb6c1fb2cb1b3095
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-05-28 00:34:40 +0200

    Time: 203 ms (5.02%), Space: 29.7 MB (28.03%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.py
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.py
@@ -1,47 +1,36 @@
 class Solution:
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
-        mod = 10 ** 9 + 7
-        all_negative = all(map(lambda x: x <= 0, arr))
-        size = len(arr)
-        if all_negative:
-          return 0
-        arr_sum = sum(arr)
-        max_sum_to_left = [(arr[0], 0)] * (2 * len(arr))
-        if k == 1:
-          cur_sum = arr[0]
-          max_sum = cur_sum
-          for v in arr[1:]:
-            cur_sum = max(cur_sum + v, v)
-            max_sum = max(max_sum, cur_sum)
-          return max_sum % mod
+        mod = 10**9 + 7
+        
+        def kadane(nums):
+            max_ending_here = max_so_far = 0
+            for x in nums:
+                max_ending_here = max(x, max_ending_here + x)
+                max_so_far = max(max_so_far, max_ending_here)
+            return max_so_far
 
-        for idx in range(1, 2 * len(arr)):
-          real_idx = idx % size
-          prev_sum, prev_sum_start = max_sum_to_left[idx - 1]
-          sum_if_add = (prev_sum, prev_sum_start + 1) if prev_sum_start + size == idx else (prev_sum + arr[real_idx], prev_sum_start)
-          sum_if_cur = (arr[real_idx], idx)
-          if sum_if_cur[0] >= sum_if_add[0]:
-            max_sum_to_left[idx] = sum_if_cur
-          else:
-            max_sum_to_left[idx] = sum_if_add
+        total_sum = sum(arr)
+        max_kadane = kadane(arr)
+        if k == 1:
+            return max_kadane % mod
+
+        max_kadane_twice = kadane(arr * 2)
         
-        max_sum_to_right = [(arr[-1], 2 * size - 1)] * (2 * len(arr))
-        for idx in range(2 * len(arr) - 2, -1, -1):
-          real_idx = idx % size
-          prev_sum, prev_sum_start = max_sum_to_right[idx + 1]
-          sum_if_add = (prev_sum, prev_sum_start - 1) if prev_sum_start - size == idx else (prev_sum + arr[real_idx], prev_sum_start)
-          sum_if_cur = (arr[real_idx], idx)
-          if sum_if_cur[0] >= sum_if_add[0]:
-            max_sum_to_right[idx] = sum_if_cur
-          else:
-            max_sum_to_right[idx] = sum_if_add
-        
-        max_sum = -inf
-        arr_sum = max(arr_sum, 0)
-        for idx, ((left_sum, left_start), (right_sum, right_start)) in enumerate(zip(max_sum_to_left, max_sum_to_right)):
-          if idx < size:
-            max_sum = max(max_sum, left_sum + arr_sum * (k - 1), right_sum + arr_sum * (k - 2))
-          else:
-            max_sum = max(max_sum, left_sum + arr_sum * (k - 2), right_sum + arr_sum * (k - 1))
-        return max_sum % mod
-        
+        prefix_sum = curr = 0
+        max_prefix = 0
+        for x in arr:
+            curr += x
+            max_prefix = max(max_prefix, curr)
+
+        suffix_sum = curr = 0
+        max_suffix = 0
+        for x in reversed(arr):
+            curr += x
+            max_suffix = max(max_suffix, curr)
+
+        if total_sum > 0:
+            result = max(max_kadane_twice, max_prefix + max_suffix + (k - 2) * total_sum)
+        else:
+            result = max(max_kadane_twice, max_kadane)
+
+        return result % mod
