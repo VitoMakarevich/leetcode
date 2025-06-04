267b98df58109243cbf27ac850f88832c4ec51ec
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-06-04 14:35:25 +0200

    Time: 0 ms (100%), Space: 17.9 MB (49.28%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.py
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.py
@@ -2,14 +2,14 @@ class Solution:
     def maxStrength(self, nums: List[int]) -> int:
       negative = deque()
       positive = deque()
-      zeros = deque()
+      contains_zero = False
       for v in nums:
         if v < 0:
           negative.append(v)
         elif v > 0:
           positive.append(v)
         else:
-          zeros.append(v)
+          contains_zero = True
       
       if len(negative) >= 2:
         negative_divider = max(negative) if len(negative) % 2 == 1 else 1
@@ -17,7 +17,7 @@ class Solution:
         return prod(negative) // negative_divider * prod(positive)
       if len(positive) > 0:
         return prod(positive)
-      if len(zeros):
+      if contains_zero:
         return 0
       return negative[0]
 
