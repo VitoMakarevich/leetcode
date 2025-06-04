3085329fca87a5a8a2c95368081801f4666d5e8d
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-06-04 03:29:00 +0200

    Time: 381 ms (33.98%), Space: 17.9 MB (79.61%) - LeetHub

--- 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.py
+++ 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.py
@@ -3,11 +3,9 @@ class Solution:
       primes_up_to_border = set(self.primes(int(sqrt(r))))
       range_size = r - l + 1
       special_count = 0
-      for i in range(2, int(sqrt(r)) + 1):
-          if i in primes_up_to_border:
-              square = i * i
-              if l <= square <= r:
-                  special_count += 1
+      for prime in primes_up_to_border:
+        special_count += int(l <= prime * prime <= r)
+
       return r - l + 1 - special_count
     
     def primes(self, right):
