3baeeed311e7232a3948f8a4f16ea3d2431bf632
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-06-04 19:27:35 +0200

    Time: 87 ms (43.7%), Space: 18 MB (25.98%) - LeetHub

--- 0866-prime-palindrome/0866-prime-palindrome.py
+++ 0866-prime-palindrome/0866-prime-palindrome.py
@@ -1,34 +1,17 @@
-class Solution:
-    def primePalindrome(self, n: int) -> int:
-      max_num_length = min(9, (len(str(n)) + 3))
-      max_num = int('9' * max_num_length)
-      
-      primes = self.get_primes(max_num)
+class Solution(object):
+    def primePalindrome(self, N):
+        def is_prime(n):
+            return n > 1 and all(n%d for d in range(2, int(n**.5) + 1))
 
-      for cand in primes:
-        if cand >= n and self.is_palindrome(cand):
-          return cand
+        for length in range(1, 6):
+            for root in range(10**(length - 1), 10**length):
+                s = str(root)
+                x = int(s + s[-2::-1])
+                if x >= N and is_prime(x):
+                    return x
 
-    def get_primes(self, top):
-      candidates = [True] * (top + 1)
-      candidates[0] = False
-      candidates[1] = False
-      for base in range(2, top + 1):
-        if candidates[base]:
-          for val in range(base * base, top + 1, base):
-            candidates[val] = False
-      return [idx for idx, v in enumerate(candidates) if v]
-    
-    def is_palindrome(self, candidate):
-      str_val = str(candidate)
-      i = 0
-      j = len(str_val) - 1
-      
-      while i <= j:
-        if str_val[i] != str_val[j]:
-          return False
-        i += 1
-        j -= 1
-      return True
-
-      
+            for root in range(10**(length - 1), 10**length):
+                s = str(root)
+                x = int(s + s[-1::-1])
+                if x >= N and is_prime(x):
+                    return x
