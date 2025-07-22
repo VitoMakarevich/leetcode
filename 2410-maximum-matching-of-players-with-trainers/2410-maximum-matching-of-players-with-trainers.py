5dcc1f81ccd9369437f7af71ea98930357b78541
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-07-22 17:24:39 +0200

    Time: 187 ms (5.04%), Space: 38.1 MB (12.11%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -1,15 +1,18 @@
 class Solution:
     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
         res = 0
-        players.sort(reverse = True)
-        trainers.sort(reverse = True)
-        p_ptr = 0
-        for t_ptr in range(len(trainers)):
-          while p_ptr < len(players) and trainers[t_ptr] < players[p_ptr]:
-            p_ptr += 1
-          if p_ptr != len(players):
+        players = [-p for p in players]
+        heapq.heapify(players)
+        trainers = [-t for t in trainers]
+        heapq.heapify(trainers)
+        while players and trainers:
+          best_trainer = -trainers[0]
+          best_player = -players[0]
+          if best_trainer >= best_player:
             res += 1
-            p_ptr += 1
+            heapq.heappop(trainers)
+            heapq.heappop(players)
           else:
-            break
+            heapq.heappop(players)
+            
         return res
