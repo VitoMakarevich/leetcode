3a6aeeaac82b6835e0b924a037dd9b9cc64a7542
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-07-22 17:11:31 +0200

    Time: 292 ms (5.01%), Space: 39.7 MB (5.36%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
@@ -11,6 +11,6 @@ class Solution:
             search = int(not bool(search))
             print(search)
         return counter
-      res = max(changing_start_from(0), changing_start_from(1), sum(1 for n in remains if n == 1), sum([0 for n in remains if n == 0]))
+      res = max(changing_start_from(0), changing_start_from(1), sum(1 for n in remains if n == 1), sum([1 for n in remains if n == 0]))
       return res
     
