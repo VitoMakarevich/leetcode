044f33f4344d01b4e7a8c754ec16382009b85c40
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-07-23 19:29:56 +0200

    Time: 4 ms (49.33%), Space: 19.6 MB (25.02%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,37 +2,33 @@ from collections import deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph_source_to_target = [set() for _ in range(numCourses)]
-        graph_target_to_source = [set() for _ in range(numCourses)]   
+        graph = [[] for _ in range(numCourses)]
 
-        for req in prerequisites:
-          target = req[0]
-          source = req[1]
-          graph_source_to_target[source].add(target)
-          graph_target_to_source[target].add(source)
-
-        visited = set()   
-        chain = deque([])
-        iteration = deque([])
-        for index, childs in enumerate(graph_target_to_source):
-          if len(graph_target_to_source[index]) == 0:
-            iteration.append(index)
-        
-        while iteration:
-          nxt = iteration.popleft()
-          if nxt in visited:
-            continue
-          visited.add(nxt)
-          chain.append(nxt)
-          for child in graph_source_to_target[nxt]:
-            graph_target_to_source[child].remove(nxt)
-            if len(graph_target_to_source[child]) == 0:
-              iteration.append(child)
-            
-        if len(visited) == numCourses:
-          return list(chain)
-        else:
-          return []
+        for target, source in prerequisites:
+          graph[target].append(source)
+        res = []
+        visited = set()
+        def dfs(idx, visiting):
+          if idx in visiting:
+            return False
+          if idx in visited:
+            return True
+          
+          visiting.add(idx)
+          for predecessor in graph[idx]:
+            cycle = dfs(predecessor, visiting)
+            if not cycle:
+              return False
+          res.append(idx)
+          visiting.remove(idx)
+          visited.add(idx)
+          return True
+        for cource in range(numCourses):
+          if cource not in visited: 
+            cycle = dfs(cource, set())
+            if not cycle:
+              return []
+        return res if len(res) == numCourses else []
         
 
     
