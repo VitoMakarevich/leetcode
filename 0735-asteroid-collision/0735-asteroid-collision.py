4f63c02328135f19721e2ce7ebcb7de706f3d460
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-07-23 15:34:36 +0200

    Time: 0 ms (100%), Space: 18.9 MB (62.26%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,23 +1,24 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        res = deque([asteroids[0]])
-        for i in range(1, len(asteroids)):
-            cur = asteroids[i]
-            while res:
-                last = res.pop()
-                if not(cur < 0 and last > 0 and abs(cur) > last):
-                    res.append(last)
-                    break
-            if res:
-                last = res[-1]
-                # if equal but different signs
-                if cur < 0 and last > 0 and cur == -last:
-                    # remove last item in deque
-                    res.pop()
-                # if both same sign or + follows -
-                elif ((last < 0 and cur < 0) or (last > 0 and cur > 0)) or (cur > 0 and last < 0):
-                    res.append(cur)
+      q = deque()
+      for ast in asteroids:
+        if not q or ast >= 0 or (q[-1] < 0 and ast < 0):
+          q.append(ast)
+        else:
+          while q:
+            if q[-1] > 0 and q[-1] == -ast:
+              q.pop()
+              break
+            elif q[-1] > 0 and q[-1] > -ast:
+              break
             else:
-                res.append(cur)
+              q.pop()
+              if not q or q[-1] < 0:
+                q.append(ast)
+                break
+          
 
-        return list(res)
+
+          
+          
+      return list(q)
