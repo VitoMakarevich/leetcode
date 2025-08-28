ed646f2576f9e9a0a2633df36a76b166c973ed61
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-08-28 11:24:32 +0200

    Time: 43 ms (32.19%), Space: 28.5 MB (14.68%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+      output = 0
+      left = 0
+      nums += [20]
+      for right, v in enumerate(nums):
+        if v == 0:
+          continue
+        while left <= right and nums[left] != 0:
+          left += 1
+
+        if left <= right and nums[left] == 0:
+          diff = right - left
+          output += ((1 + diff) * diff) // 2
+          left = right
+      return output
