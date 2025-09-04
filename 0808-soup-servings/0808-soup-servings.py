73bb15877a1134c648bb8549f3a5eae8157bdd02
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-04 15:45:48 +0200

    Time: 722 ms (9.75%), Space: 20.4 MB (22.6%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -6,10 +6,11 @@ class Solution:
       decrements = [(4, 0), (3, 1), (2, 2), (1, 3)]
       
 
-      def calc_dp(i, j):
-            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
-                    dp[max(0, i - 2)][max(0, j - 2)]
-                    + dp[i - 1][max(0, j - 3)]) / 4
+      def calc_dp(a, b):
+        total_prob = 0
+        for a_dec, b_dec in decrements:
+          total_prob += dp[max(0, a - a_dec)][max(0, b - b_dec)]
+        return total_prob / 4
 
       dp[0] = {0: 0.5}
       for k in range(1, n + 1):
