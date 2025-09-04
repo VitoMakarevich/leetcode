8721195488601a2195d7babf1e7e4aee415b2f13
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-04 15:44:52 +0200

    Time: 723 ms (9.75%), Space: 20.3 MB (26.72%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -2,24 +2,23 @@ class Solution:
     
     def soupServings(self, n: int) -> float:
       n = ceil(n / 25)
-      self.changes = [(4, 0), (3, 1), (2, 2), (1, 3)]
+      dp = {}
+      decrements = [(4, 0), (3, 1), (2, 2), (1, 3)]
+      
+
+      def calc_dp(a, b):
+        total_prob = 0
+        for a_dec, b_dec in decrements:
+          total_prob += dp[max(0, a - a_dec)][max(0, b - b_dec)]
+        return total_prob / 4
+
+      dp[0] = {0: 0.5}
       for k in range(1, n + 1):
-        if self.dp(k, k) > 1 - 1e-5:
+        dp[0][k] = 1
+        dp[k] = {0: 0}
+        for j in range(1, k + 1):
+          dp[j][k] = calc_dp(j, k)
+          dp[k][j] = calc_dp(k, j)
+        if dp[k][k] > 1 - 1e-5:
           return 1
-      return self.dp(n, n)
-    
-    @cache
-    def dp(self, a, b):
-      if a <= 0 or b <= 0:
-        return self.multipliers(a, b)
-      res = 0
-      for a_minus, b_minus in self.changes:
-        res += self.dp(a - a_minus, b - b_minus)
-      return res / 4
-    
-    def multipliers(self, a, b):
-      if a <= 0 and b > 0:
-        return 1
-      elif a <= 0 and b <= 0:
-        return 0.5
-      return 0
+      return dp[n][n]
