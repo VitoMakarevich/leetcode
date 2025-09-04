9b893f2105262420c079ab3c2fd1a0cbcb8c89c7
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-04 15:46:38 +0200

    Time: 11 ms (62.97%), Space: 19.5 MB (58.39%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -1,11 +1,11 @@
 class Solution:
     
     def soupServings(self, n: int) -> float:
+      if n > 5000:
+        return 1
       n = ceil(n / 25)
       self.changes = [(4, 0), (3, 1), (2, 2), (1, 3)]
-      for k in range(1, n + 1):
-        if self.dp(k, k) > 1 - 1e-5:
-          return 1
+      
       return self.dp(n, n)
     
     @cache
