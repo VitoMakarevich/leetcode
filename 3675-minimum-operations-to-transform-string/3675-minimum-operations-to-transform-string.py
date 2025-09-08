eef1717effafdcc38bcc015f58d09ff2b1d85187
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-08 20:13:22 +0200

    Time: 58 ms (81.6%), Space: 20.1 MB (100%) - LeetHub

--- 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.py
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def minOperations(self, s: str) -> int:
       smallest = inf
-      max_chars = 26
-      start_char = ord('a')
-      for c in s:
-        if c == 'a':
-          pos = max_chars
-        else:
-          pos = ord(c) - start_char
-        smallest = min(smallest, pos)
-      return max_chars - smallest
+      all_chars = set(s)
+      ordered = sorted(list(all_chars))
+      if ordered[0] == 'a' and len(ordered) == 1:
+        return 0
+      elif ordered[0] == 'a':
+        return 26 - (ord(ordered[1]) - ord('a'))
+      else:
+        return 26 - (ord(ordered[0]) - ord('a'))
+
+      
