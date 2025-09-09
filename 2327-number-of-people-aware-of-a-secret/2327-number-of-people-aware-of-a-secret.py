362a90e597624af2447b67d78bbc853d618c04bb
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-09 21:35:32 +0200

    Time: 325 ms (18.84%), Space: 18.1 MB (36.23%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -7,13 +7,13 @@ class Solution:
       dp[0]['share'] = 1
       for i in range(n):
         new = dp[i]['share'] - dp[i]['forget'] if i != 0 else new
-        cur = max(cur + new, 0) % mod
-        if cur == 0:
-          return 0
+        cur = (cur + new) % mod
+        # if cur == 0:
+        #   return 0
         share_up_to = min(n, i + forget)
         for share_point in range(i + delay, share_up_to):
           dp[share_point]['share'] += dp[i]['share']
         if i + forget < n:
           dp[i + forget]['forget'] += dp[i]['share']
-        # print(f'dp={dp[i]}, cur={cur}, new={new}')
+
       return cur
