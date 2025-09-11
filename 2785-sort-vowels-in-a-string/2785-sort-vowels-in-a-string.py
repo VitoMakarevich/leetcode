34357a1be84bb619f57ff81327d2e4c4775974b6
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-11 17:03:15 +0200

    Time: 82 ms (60.39%), Space: 20.6 MB (53.03%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,15 +1,16 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
       s = list(s)
-      vowels_set = set(['a', 'e', 'i', 'o', 'u'])
+      vowels_lower = ['a', 'e', 'i', 'o', 'u']
+      vowels_set = set(vowels_lower + [c.upper() for c in vowels_lower])
       vowels_in_str = []
       for c in s:
-        if c.lower() in vowels_set:
+        if c in vowels_set:
           vowels_in_str.append(c)
       vowels_in_str.sort()
       ptr = 0
       for i, c in enumerate(s):
-        if c.lower() in vowels_set:
+        if c in vowels_set:
           s[i] = vowels_in_str[ptr]
           ptr += 1
       return ''.join(s)
