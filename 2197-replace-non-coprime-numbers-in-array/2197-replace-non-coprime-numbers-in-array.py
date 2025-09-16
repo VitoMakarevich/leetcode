46a667b6fe9d9c530540a6001527e13f30018684
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-09-16 12:23:21 +0200

    Time: 246 ms (18.48%), Space: 32.1 MB (18.48%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -7,10 +7,7 @@ class Solution:
 
       while idx < len(nums):
         non_coprime = gcd(nums[idx], stack[-1]) > 1
-        if non_coprime:
-          stack.append(lcm(stack.pop(), nums[idx]))
-        else:
-          stack.append(nums[idx])
+        stack.append(nums[idx])
         idx += 1
         while len(stack) >= 2 and gcd(stack[-1], stack[-2]) > 1:
           stack.append(lcm(stack.pop(), stack.pop()))
