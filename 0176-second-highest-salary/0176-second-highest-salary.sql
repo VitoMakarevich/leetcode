c1526e21cd2b7d2bd120a9834fe5e5ca17b69fee
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-10-02 20:25:17 +0200

    Time: 198 ms (78.26%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,12 @@
-SELECT MAX(salary) AS "SecondHighestSalary"
-FROM (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    OFFSET 1 LIMIT 1
-) sub;
+with distinct_salaries as (
+  select distinct salary from Employee
+),
+salaries_with_rank as (
+  select salary, (dense_rank() over (order by salary desc)) "rank" from distinct_salaries
+),
+second_salary_with_null as (
+  select salary from salaries_with_rank where "rank" = 2
+  union
+  select null
+)
+select salary "SecondHighestSalary" from second_salary_with_null order by salary nulls last limit 1
