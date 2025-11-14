7e9af4e0b00b0f8cb0f3ad44089ef3217637ef59
Vitali Makarevich <vitaliy.makarevich.work@gmail.com>
2025-11-14 20:50:38 +0100

    Time: 248 ms (48.05%), Space: 41.5 MB (21.65%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -1,17 +1,21 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-      res = [[0] * n for _ in range(n)]
-      sweep = [[0] * n for _ in range(n)]
+      res = [[0] * (n) for _ in range(n)]
+      sweep = [[0] * (n + 1) for _ in range(n + 1)]
       for lt_i, lt_j, rb_i, rb_j in queries:
+        sweep[lt_i][lt_j] += 1
+        sweep[rb_i + 1][rb_j + 1] += 1 
+        if rb_i + 1 < n:
+          sweep[rb_i + 1][lt_j] -= 1
+        if rb_j + 1 < n:
+          sweep[lt_i][rb_j + 1] -= 1
 
-        for row in range(lt_i, rb_i + 1):
-          sweep[row][lt_j] += 1
-          if rb_j + 1 < n:
-            sweep[row][rb_j + 1] -= 1
       
+      print(sweep)
       for row in range(n):
-        cur = 0
         for col in range(n):
-          cur += sweep[row][col]
-          res[row][col] = cur
+          top = 0 if row == 0 else res[row - 1][col]
+          left = 0 if col == 0 else res[row][col - 1]
+          top_left = 0 if col == 0 or row == 0 else res[row - 1][col - 1]
+          res[row][col] = sweep[row][col] + top + left - top_left
       return res
